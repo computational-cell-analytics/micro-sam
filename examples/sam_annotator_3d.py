6acb991a6b2a273b165b0f1cdf1ebdfe3d31a79c
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2023-05-03 22:25:17 +0200

    Update annotator 3d example

--- examples/sam_annotator_3d.py
+++ examples/sam_annotator_3d.py
@@ -1,23 +1,13 @@
-import z5py
+from elf.io import open_file
 from micro_sam.sam_annotator import annotator_3d
 
 
-def _load_raw(input_path, halo=[40, 384, 384], center=None):
-    with z5py.File(input_path, "r") as f:
-        ds = f["raw"]
-        shape = ds.shape
-        center = [sh // 2 for sh in shape] if center is None else center
-        bb = tuple(slice(ce - ha, ce + ha) for ce, ha in zip(center, halo))
-        raw = ds[bb]
-    print("Loaded raw from bb:", bb)
-    return raw
-
-
+# Lucchi++ Data from: https://casser.io/connectomics/
 def main():
-    halo = [40, 384, 384]
-    input_path = "/home/pape/Work/data/moebius/mito/4007.n5"
-    raw = _load_raw(input_path, halo)
-    embedding_path = "./embeddings/embeddings-carving-mito.zarr"
+    input_path = "./data/Lucchi++/Test_In"
+    with open_file(input_path) as f:
+        raw = f["*.png"][:]
+    embedding_path = "./embeddings/embeddings-lucchi.zarr"
     annotator_3d(raw, embedding_path, show_embeddings=False)
 
 
