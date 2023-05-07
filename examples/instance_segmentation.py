e44ce8283c657314c3f4ee5d42ad40c6105107b2
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2023-05-07 19:27:48 +0200

    Update example scripts

--- examples/instance_segmentation.py
+++ examples/instance_segmentation.py
@@ -15,7 +15,7 @@ def mito_segmentation():
     image_embeddings = util.precompute_image_embeddings(predictor, raw, "./embeddings/embeddings-mito2d.zarr")
     embedding_pca = compute_pca(image_embeddings["features"])
 
-    seg, initial_seg = segment_from_embeddings(predictor, image_embeddings, return_initial_seg=True)
+    seg, initial_seg = segment_from_embeddings(predictor, image_embeddings=image_embeddings, return_initial_seg=True)
 
     v = napari.Viewer()
     v.add_image(raw)
@@ -25,10 +25,35 @@ def mito_segmentation():
     napari.run()
 
 
+def cell_segmentation():
+    path = "./DIC-C2DH-HeLa/train/01"
+    with open_file(path, mode="r") as f:
+        timeseries = f["*.tif"][:50]
+
+    frame = 11
+
+    predictor = util.get_sam_model()
+    image_embeddings = util.precompute_image_embeddings(predictor, timeseries, "./embeddings/embeddings-ctc.zarr")
+    embedding_pca = compute_pca(image_embeddings["features"][frame])
+
+    seg, initial_seg = segment_from_embeddings(
+        predictor, image_embeddings=image_embeddings, i=frame, return_initial_seg=True
+    )
+
+    v = napari.Viewer()
+    v.add_image(timeseries[frame])
+    v.add_image(embedding_pca, scale=(8, 8))
+    v.add_labels(seg)
+    v.add_labels(initial_seg)
+    napari.run()
+
+
 def main():
-    mito_segmentation()
-    # TODO set up cell segmentation, e.g. on CTC Hela data or some IncuCyte data
-    # cell_segmentation()
+    # automatic segmentation for the data from Lucchi et al. (see 'sam_annotator_3d.py')
+    # mito_segmentation()
+
+    # automatic segmentation for data from the cell tracking challenge (see 'sam_annotator_tracking.py')
+    cell_segmentation()
 
 
 if __name__ == "__main__":
--- examples/sam_annotator_tracking.py
+++ examples/sam_annotator_tracking.py
@@ -1,22 +1,39 @@
 from glob import glob
 
-import h5py
 import numpy as np
+from elf.io import open_file
 from micro_sam.sam_annotator import annotator_tracking
 
 
-def main():
+def track_incucyte_data():
     pattern = "/home/pape/Work/data/incu_cyte/carmello/videos/MiaPaCa_flat_B3-3_registered/image-*"
     paths = glob(pattern)
     paths.sort()
 
     timeseries = []
     for p in paths[:45]:
-        with h5py.File(p) as f:
+        with open_file(p, mode="r") as f:
             timeseries.append(f["phase-contrast"][:])
     timeseries = np.stack(timeseries)
 
     annotator_tracking(timeseries, embedding_path="./embeddings/embeddings-tracking.zarr", show_embeddings=False)
+
+
+# TODO describe how to get the data from CTC
+def track_ctc_data():
+    path = "./data/DIC-C2DH-HeLa/train/01"
+    with open_file(path, mode="r") as f:
+        timeseries = f["*.tif"][:50]
+
+    annotator_tracking(timeseries, embedding_path="./embeddings/embeddings-ctc.zarr")
+
+
+def main():
+    # private data used for initial tests
+    # track_incucyte_data()
+
+    # data from the cell tracking challenges
+    track_ctc_data()
 
 
 if __name__ == "__main__":
