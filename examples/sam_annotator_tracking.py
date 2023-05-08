43241633872b1b0c9e9fa6e96eb47560e5113fb8
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2023-05-08 16:28:09 +0200

    Add data explanation to the tracking example

--- examples/sam_annotator_tracking.py
+++ examples/sam_annotator_tracking.py
@@ -2,7 +2,9 @@ from elf.io import open_file
 from micro_sam.sam_annotator import annotator_tracking
 
 
-# TODO describe how to get the data from CTC
+# This runs the interactive tracking annotator for data from the cell tracking challenge:
+# It uses the training data for the HeLA dataset. You can download the data via
+# http://data.celltrackingchallenge.net/training-datasets/DIC-C2DH-HeLa.zip
 def track_ctc_data():
     path = "./data/DIC-C2DH-HeLa/train/01"
     with open_file(path, mode="r") as f:
