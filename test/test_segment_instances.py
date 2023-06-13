d43aa150885b2ef13da47b6b4316fdb7cae013df
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2023-06-14 01:22:58 +0200

    Implement test for automatic instance segmentation

--- test/test_segment_instances.py
+++ test/test_segment_instances.py
@@ -5,12 +5,13 @@ import numpy as np
 
 from elf.evaluation.matching import matching
 from skimage.draw import disk
+from skimage.measure import label
 
 
 class TestSegmentInstances(unittest.TestCase):
 
     # create an input image with three objects
-    def _get_input(self, shape=(96, 96)):
+    def _get_input(self, shape=(512, 512)):
         mask = np.zeros(shape, dtype="uint8")
 
         def write_object(center, radius):
@@ -18,15 +19,16 @@ class TestSegmentInstances(unittest.TestCase):
             mask[circle] = 1
 
         center = tuple(sh // 4 for sh in shape)
-        write_object(center, radius=8)
+        write_object(center, radius=19)
 
         center = tuple(sh // 2 for sh in shape)
-        write_object(center, radius=9)
+        write_object(center, radius=27)
 
         center = tuple(3 * sh // 4 for sh in shape)
-        write_object(center, radius=7)
+        write_object(center, radius=22)
 
         image = mask * 255
+        mask = label(mask)
         return mask, image
 
     def _get_model(self):
@@ -43,7 +45,6 @@ class TestSegmentInstances(unittest.TestCase):
         predicted = segment_instances_sam(sam, image)
         self.assertGreater(matching(predicted, mask, threshold=0.75)["precision"], 0.99)
 
-    @unittest.skip("Needs some more debugging.")
     def test_segment_instances_from_embeddings(self):
         from micro_sam.segment_instances import segment_instances_from_embeddings
 
@@ -53,13 +54,10 @@ class TestSegmentInstances(unittest.TestCase):
         image_embeddings = util.precompute_image_embeddings(predictor, image)
         util.set_precomputed(predictor, image_embeddings)
 
-        predicted = segment_instances_from_embeddings(predictor, image_embeddings)
-        # import napari
-        # v = napari.Viewer()
-        # v.add_image(image)
-        # v.add_labels(mask)
-        # v.add_labels(predicted)
-        # napari.run()
+        predicted = segment_instances_from_embeddings(
+            predictor, image_embeddings, min_initial_size=0, with_background=True
+        )
+
         self.assertGreater(matching(predicted, mask, threshold=0.75)["precision"], 0.99)
 
 
