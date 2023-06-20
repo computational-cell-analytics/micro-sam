d9f69f946708428a38135777186287b4db93713e
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2023-06-20 19:19:21 +0200

    Add test scripts for 3d instance segmentation

--- development/instance_segmentation.py
+++ development/instance_segmentation.py
@@ -5,44 +5,71 @@ import micro_sam.util as util
 from micro_sam.segment_instances import (
     segment_instances_from_embeddings,
     segment_instances_sam,
-    # segment_instances_from_embeddings_with_tiling
+    segment_instances_from_embeddings_3d,
 )
 from micro_sam.visualization import compute_pca
 
-INPUT_PATH = "../examples/data/Lucchi++/Test_In"
-EMBEDDINGS_PATH = "../examples/embeddings/embeddings-mito2d.zarr"
+INPUT_PATH = "/home/pape/Work/data/mouse-embryo/Nuclei/for_sam.h5"
+EMBEDDINGS_PATH = "./embeddings/embedding-nuclei-3d.zarr"
 TIMESERIES_PATH = "../examples/data/DIC-C2DH-HeLa/train/01"
 EMBEDDINGS_TRACKING_PATH = "../examples/embeddings/embeddings-ctc.zarr"
 
 
-def mito_segmentation() -> None:
-    """Performs mito segmentation on the input image."""
+def nucleus_segmentation(use_sam=False, use_mws=False) -> None:
+    """Segment nuclei in 3d lightsheet data (one slice)."""
     with open_file(INPUT_PATH) as f:
-        raw = f["*.png"][-1, :768, :768]
+        raw = f["raw"][:]
 
-    predictor, sam = util.get_sam_model(return_sam=True)
+    z = 32
 
-    print("Run SAM prediction ...")
-    seg_sam = segment_instances_sam(sam, raw)
+    predictor, sam = util.get_sam_model(return_sam=True, model_type="vit_b")
+    if use_sam:
+        print("Run SAM prediction ...")
+        seg_sam = segment_instances_sam(sam, raw[z])
+    else:
+        seg_sam = None
 
     image_embeddings = util.precompute_image_embeddings(predictor, raw, EMBEDDINGS_PATH)
-    embedding_pca = compute_pca(image_embeddings["features"])
+    embedding_pca = compute_pca(image_embeddings["features"])[z]
 
-    print("Run prediction from embeddings ...")
-    seg, initial_seg = segment_instances_from_embeddings(
-        predictor, image_embeddings=image_embeddings, return_initial_seg=True
-    )
+    if use_mws:
+        print("Run prediction from embeddings ...")
+        seg, initial_seg = segment_instances_from_embeddings(
+            predictor, image_embeddings=image_embeddings, return_initial_segmentation=True,
+            pred_iou_thresh=0.8, verbose=1, stability_score_thresh=0.9,
+            i=z,
+        )
+    else:
+        seg, initial_seg = None, None
 
     v = napari.Viewer()
-    v.add_image(raw)
-    v.add_image(embedding_pca, scale=(12, 12))
-    v.add_labels(seg_sam)
-    v.add_labels(seg)
-    v.add_labels(initial_seg)
+    v.add_image(raw[z])
+    v.add_image(embedding_pca, scale=(12, 12), visible=False)
+    if seg_sam is not None:
+        v.add_labels(seg_sam)
+    if seg is not None:
+        v.add_labels(seg)
+    if initial_seg is not None:
+        v.add_labels(initial_seg, visible=False)
     napari.run()
 
 
-def cell_segmentation(use_sam=False, use_mws=False, use_tiling=False) -> None:
+def nucleus_segmentation_3d() -> None:
+    """Segment nuclei in 3d lightsheet data (3d segmentation)."""
+    with open_file(INPUT_PATH) as f:
+        raw = f["raw"][:]
+
+    predictor = util.get_sam_model(model_type="vit_b")
+    image_embeddings = util.precompute_image_embeddings(predictor, raw, EMBEDDINGS_PATH)
+    seg = segment_instances_from_embeddings_3d(predictor, image_embeddings)
+
+    v = napari.Viewer()
+    v.add_image(raw)
+    v.add_labels(seg)
+    napari.run()
+
+
+def cell_segmentation(use_sam=False, use_mws=False) -> None:
     """Performs cell segmentation on the input timeseries."""
     with open_file(TIMESERIES_PATH, mode="r") as f:
         timeseries = f["*.tif"][:50]
@@ -50,16 +77,15 @@ def cell_segmentation(use_sam=False, use_mws=False, use_tiling=False) -> None:
     frame = 11
     predictor, sam = util.get_sam_model(return_sam=True)
 
-    image_embeddings = util.precompute_image_embeddings(
-        predictor, timeseries, EMBEDDINGS_TRACKING_PATH)
+    image_embeddings = util.precompute_image_embeddings(predictor, timeseries, EMBEDDINGS_TRACKING_PATH)
 
     embedding_pca = compute_pca(image_embeddings["features"][frame])
 
     if use_mws:
         print("Run embedding segmentation ...")
         seg_mws, initial_seg = segment_instances_from_embeddings(
-            predictor, image_embeddings=image_embeddings, i=frame, return_initial_seg=True,
-            bias=0.0, distance_type="l2",
+            predictor, image_embeddings=image_embeddings, i=frame, return_initial_segmentation=True,
+            bias=0.0, distance_type="l2", verbose=2,
         )
     else:
         seg_mws = None
@@ -70,14 +96,6 @@ def cell_segmentation(use_sam=False, use_mws=False, use_tiling=False) -> None:
         seg_sam = segment_instances_sam(sam, timeseries[frame])
     else:
         seg_sam = None
-
-    if use_tiling:
-        print("Run embedding segmentation with tiling ...")
-        seg_tiled = segment_instances_from_embeddings_with_tiling(
-            predictor, timeseries[frame], image_embeddings
-        )
-    else:
-        seg_tiled = None
 
     v = napari.Viewer()
     v.add_image(timeseries[frame])
@@ -92,19 +110,32 @@ def cell_segmentation(use_sam=False, use_mws=False, use_tiling=False) -> None:
     if seg_sam is not None:
         v.add_labels(seg_sam)
 
-    if seg_tiled is not None:
-        v.add_labels(seg_tiled)
+    napari.run()
 
+
+def cell_segmentation_3d() -> None:
+    with open_file(TIMESERIES_PATH, mode="r") as f:
+        timeseries = f["*.tif"][:50]
+
+    predictor = util.get_sam_model()
+    image_embeddings = util.precompute_image_embeddings(predictor, timeseries, EMBEDDINGS_TRACKING_PATH)
+
+    seg = segment_instances_from_embeddings_3d(predictor, image_embeddings)
+
+    v = napari.Viewer()
+    v.add_image(timeseries)
+    v.add_labels(seg)
     napari.run()
 
 
 def main():
     # automatic segmentation for the data from Lucchi et al. (see 'sam_annotator_3d.py')
-    # mito_segmentation()
+    # nucleus_segmentation(use_mws=True)
+    nucleus_segmentation_3d()
 
     # automatic segmentation for data from the cell tracking challenge (see 'sam_annotator_tracking.py')
     # cell_segmentation(use_mws=True)
-    cell_segmentation(use_mws=True)
+    # cell_segmentation_3d()
 
 
 if __name__ == "__main__":
