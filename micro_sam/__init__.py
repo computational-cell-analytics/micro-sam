a3713f7b15208533d66ba00ff90811f05f3c3e5a
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2023-07-29 14:54:05 +0200

    Include sam_annotator in build againa and try to make doc CI script run with napari

--- micro_sam/__init__.py
+++ micro_sam/__init__.py
@@ -6,12 +6,3 @@ from .__version__ import __version__
 .. include:: ../doc/annotation_tools.md
 .. include:: ../doc/python_library.md
 """
-
-__all__ = [
-    "instance_segmentation",
-    "prompt_based_segmentation",
-    "prompt_generators",
-    "sample_data",
-    "util",
-    "visualization",
-]
