4f58b2316f840d8079bb5b9b9c00a54b1194adea
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2023-07-31 14:36:13 +0200

    Add the preliminary finetuned EM models

--- examples/annotator_3d.py
+++ examples/annotator_3d.py
@@ -3,20 +3,31 @@ from micro_sam.sam_annotator import annotator_3d
 from micro_sam.sample_data import fetch_3d_example_data
 
 
-def em_3d_annotator():
+def em_3d_annotator(use_finetuned_model):
     """Run the 3d annotator for an example EM volume."""
     # download the example data
     example_data = fetch_3d_example_data("./data")
     # load the example data (load the sequence of tif files as 3d volume)
     with open_file(example_data) as f:
         raw = f["*.png"][:]
+
+    if use_finetuned_model:
+        embedding_path = "./embeddings/embeddings-lucchi-vit_h_em.zarr"
+        model_type = "vit_h_em"
+    else:
+        embedding_path = "./embeddings/embeddings-lucchi.zarr"
+        model_type = "vit_h"
+
     # start the annotator, cache the embeddings
-    embedding_path = "./embeddings/embeddings-lucchi.zarr"
-    annotator_3d(raw, embedding_path, show_embeddings=False)
+    annotator_3d(raw, embedding_path, model_type=model_type, show_embeddings=False)
 
 
 def main():
-    em_3d_annotator()
+    # whether to use the fine-tuned SAM model
+    # this feature is still experimental!
+    use_finetuned_model = False
+
+    em_3d_annotator(use_finetuned_model)
 
 
 if __name__ == "__main__":
