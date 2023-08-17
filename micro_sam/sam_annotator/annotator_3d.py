0dc6cacdd82b891ab83a1add5a6c10fbf0fe174e
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2023-08-17 09:25:54 +0200

    Create central default model

--- micro_sam/sam_annotator/annotator_3d.py
+++ micro_sam/sam_annotator/annotator_3d.py
@@ -192,7 +192,7 @@ def annotator_3d(
     embedding_path: Optional[str] = None,
     show_embeddings: bool = False,
     segmentation_result: Optional[np.ndarray] = None,
-    model_type: str = "vit_h",
+    model_type: str = util._DEFAULT_MODEL,
     tile_shape: Optional[Tuple[int, int]] = None,
     halo: Optional[Tuple[int, int]] = None,
     return_viewer: bool = False,
