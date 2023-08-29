5fe968ceb16a27ee5f086e5722e78a1b3ed6029f
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2023-08-29 17:34:36 +1000

    Fix version string import

--- micro_sam/__init__.py
+++ micro_sam/__init__.py
@@ -5,22 +5,4 @@
 .. include:: ../doc/python_library.md
 """
 
-__version__ = "0.1.2.post1"
-
-from .sample_data import (
-    sample_data_image_series,
-    sample_data_wholeslide,
-    sample_data_livecell,
-    sample_data_hela_2d,
-    sample_data_3d,
-    sample_data_tracking,
-)
-
-__all__ = (
-    "sample_data_image_series",
-    "sample_data_wholeslide",
-    "sample_data_livecell",
-    "sample_data_hela_2d",
-    "sample_data_3d",
-    "sample_data_tracking",
-)
+from .__version__ import __version__
