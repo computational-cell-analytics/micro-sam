a4e3b293bd3facdce1c043b5a7cbd76ba585e8cf
anwai98 <anwai.archit@gmail.com>
2023-10-13 14:53:15 +0200

    Add finetuning for lm generalists

---
+++ finetuning/generalists/training/light_microscopy/obtain_lm_datasets.py
@@ -0,0 +1,109 @@
+import os
+import numpy as np
+from math import ceil, floor
+
+import torch
+
+import torch_em
+import torch_em.data.datasets as datasets
+from torch_em.transform.label import label_consecutive
+from torch_em.data import MinInstanceSampler, ConcatDataset
+from torch_em.transform.raw import standardize, normalize_percentile
+
+
+def neurips_raw_trafo(raw):
+    raw = datasets.neurips_cell_seg.to_rgb(raw)  # ensures 3 channels for the neurips data
+    raw = normalize_percentile(raw)
+    raw = np.mean(raw, axis=0)
+    raw = standardize(raw)
+    return raw
+
+
+def tissuenet_raw_trafo(raw, desired_shape=(512, 512)):
+    raw = normalize_percentile(raw, axis=(1, 2))
+    raw = np.mean(raw, axis=0)
+    raw = standardize(raw)
+
+    tmp_ddim = (desired_shape[0] - raw.shape[0], desired_shape[1] - raw.shape[1])
+    ddim = (tmp_ddim[0] / 2, tmp_ddim[1] / 2)
+    raw = np.pad(raw, pad_width=((ceil(ddim[0]), floor(ddim[0])), (ceil(ddim[1]), floor(ddim[1]))), mode="reflect")
+    assert raw.shape == desired_shape
+    return raw
+
+
+def tissuenet_label_trafo(labels, desired_shape=(512, 512)):
+    labels = label_consecutive(labels)
+
+    tmp_ddim = (desired_shape[0] - labels.shape[0], desired_shape[1] - labels.shape[1])
+    ddim = (tmp_ddim[0] / 2, tmp_ddim[1] / 2)
+    labels = np.pad(
+        labels,
+        pad_width=((ceil(ddim[0]), floor(ddim[0])), (ceil(ddim[1]), floor(ddim[1]))),
+        mode="constant"
+    )
+    assert labels.shape == desired_shape
+    return labels
+
+
+def raw_padding_trafo(raw, desired_shape=(512, 512)):
+    tmp_ddim = (desired_shape[0] - raw.shape[0], desired_shape[1] - raw.shape[1])
+    ddim = (tmp_ddim[0] / 2, tmp_ddim[1] / 2)
+    raw = np.pad(raw, pad_width=((ceil(ddim[0]), floor(ddim[0])), (ceil(ddim[1]), floor(ddim[1]))), mode="reflect")
+    assert raw.shape == desired_shape
+    return raw
+
+
+def get_concat_datasets(input_path, patch_shape, split_choice):
+    assert split_choice in ["train", "val"]
+
+    label_dtype = torch.int64
+    label_transform = label_consecutive
+    sampler = MinInstanceSampler()
+
+    generalist_dataset = ConcatDataset(
+        datasets.get_tissuenet_dataset(
+            path=os.path.join(input_path, "tissuenet"), split=split_choice,
+            patch_shape=patch_shape if split_choice == "train" else (256, 256), raw_channel="rgb",
+            label_channel="cell", raw_transform=tissuenet_raw_trafo, label_transform=tissuenet_label_trafo,
+            sampler=sampler, label_dtype=label_dtype, n_samples=1000 if split_choice == "train" else 100
+        ),
+        datasets.get_livecell_dataset(
+            path=os.path.join(input_path, "livecell"), split=split_choice, patch_shape=patch_shape,
+            label_transform=label_transform, sampler=sampler, label_dtype=label_dtype,
+            n_samples=1000 if split_choice == "train" else 100
+        ),
+        datasets.get_deepbacs_dataset(
+            path=os.path.join(input_path, "deepbacs"), split=split_choice if split_choice == "train" else "test",
+            patch_shape=patch_shape, label_transform=label_transform, sampler=sampler, label_dtype=label_dtype
+        ),
+        datasets.get_neurips_cellseg_supervised_dataset(
+            root=os.path.join(input_path, "neurips-cell-seg"), split=split_choice, patch_shape=patch_shape,
+            raw_transform=neurips_raw_trafo, label_transform=label_transform, label_dtype=label_dtype, sampler=sampler
+        ),
+        datasets.get_dsb_dataset(
+            path=os.path.join(input_path, "dsb"), split=split_choice if split_choice == "train" else "test",
+            patch_shape=(1, patch_shape[0], patch_shape[1]), label_transform=label_transform, sampler=sampler,
+            label_dtype=label_dtype
+        ),
+        datasets.get_plantseg_dataset(
+            path=os.path.join(input_path, "plantseg"), name="root", sampler=MinInstanceSampler(min_num_instances=10),
+            label_transform=tissuenet_label_trafo, ndim=2, split=split_choice, label_dtype=label_dtype,
+            raw_transform=raw_padding_trafo, patch_shape=(1, patch_shape[0], patch_shape[1]),
+            n_samples=1000 if split_choice == "train" else 100
+        )
+
+    )
+    generalist_dataset.datasets[3].max_sampling_attempts = 5000
+
+    return generalist_dataset
+
+
+def get_generalist_lm_loaders(input_path, patch_shape):
+    """This returns the concatenated light microscopy datasets implemented in torch_em:
+    https://github.com/constantinpape/torch-em/tree/main/torch_em/data/datasets
+    """
+    generalist_train_dataset = get_concat_datasets(input_path, patch_shape, "train")
+    generalist_val_dataset = get_concat_datasets(input_path, patch_shape, "val")
+    train_loader = torch_em.get_data_loader(generalist_train_dataset, batch_size=2, shuffle=True, num_workers=16)
+    val_loader = torch_em.get_data_loader(generalist_val_dataset, batch_size=1, shuffle=True, num_workers=16)
+    return train_loader, val_loader
---
+++ finetuning/generalists/training/light_microscopy/train_lm_generalist.py
@@ -0,0 +1,98 @@
+import os
+import argparse
+
+import torch
+
+from torch_em.loss import DiceLoss
+
+import micro_sam.training as sam_training
+from micro_sam.util import export_custom_sam_model
+
+from .obtain_lm_datasets import get_generalist_lm_loaders
+
+
+def finetune_lm_generalist(args):
+    """Example code for finetuning SAM on multiple light microscopy datasets"""
+    # override this (below) if you have some more complex set-up and need to specify the exact gpu
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    # training settings:
+    model_type = args.model_type
+    checkpoint_path = None  # override this to start training from a custom checkpoint
+    patch_shape = (512, 512)  # the patch shape for training
+    n_objects_per_batch = 25  # this is the number of objects per batch that will be sampled
+    freeze_parts = None  # override this to freeze one or more of these backbones
+
+    # get the trainable segment anything model
+    model = sam_training.get_trainable_sam_model(model_type, checkpoint_path, freeze_parts)
+
+    # all stuff needed for training
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.9, patience=10, verbose=True)
+    train_loader, val_loader = get_generalist_lm_loaders(args.input_path, patch_shape)
+
+    # this class creates all the training data for a batch (inputs, prompts and labels)
+    convert_inputs = sam_training.ConvertToSamInputs()
+
+    checkpoint_name = "generalist_lm_sam"
+    # the trainer which performs training and validation (implemented using "torch_em")
+    trainer = sam_training.SamTrainer(
+        name=checkpoint_name,
+        save_root=args.save_root,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        model=model,
+        optimizer=optimizer,
+        # currently we compute loss batch-wise, else we pass channelwise True
+        loss=DiceLoss(channelwise=False),
+        metric=DiceLoss(),
+        device=device,
+        lr_scheduler=scheduler,
+        logger=sam_training.SamLogger,
+        log_image_interval=100,
+        mixed_precision=True,
+        convert_inputs=convert_inputs,
+        n_objects_per_batch=n_objects_per_batch,
+        n_sub_iteration=8,
+        compile_model=False
+    )
+    trainer.fit(iterations=args.iterations)
+    if args.export_path is not None:
+        checkpoint_path = os.path.join(
+            "" if args.save_root is None else args.save_root, "checkpoints", checkpoint_name, "best.pt"
+        )
+        export_custom_sam_model(
+            checkpoint_path=checkpoint_path,
+            model_type=model_type,
+            save_path=args.export_path,
+        )
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Finetune Segment Anything for the LM datasets.")
+    parser.add_argument(
+        "--input_path", "-i", default="/scratch/usr/nimanwai/data/",
+        help="The filepath to the LM datasets. If the data does not exist yet it will be downloaded"  # TODO: check this
+    )
+    parser.add_argument(
+        "--model_type", "-m", default="vit_b",
+        help="The model type to use for fine-tuning. Either vit_h, vit_b or vit_l."
+    )
+    parser.add_argument(
+        "--save_root", "-s",
+        help="Where to save the checkpoint and logs. By default they will be saved where this script is run from."
+    )
+    parser.add_argument(
+        "--iterations", type=int, default=int(1e5),
+        help="For how many iterations should the model be trained? By default 100k."
+    )
+    parser.add_argument(
+        "--export_path", "-e",
+        help="Where to export the finetuned model to. The exported model can be used in the annotation tools."
+    )
+    args = parser.parse_args()
+    finetune_lm_generalist(args)
+
+
+if __name__ == "__main__":
+    main()
