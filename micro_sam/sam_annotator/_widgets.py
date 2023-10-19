38734f063422aea51f37028241a5fdcd2f44f3e2
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2023-10-19 14:43:34 +1100

    Embedding widget for napari plugin

--- micro_sam/sam_annotator/_widgets.py
+++ micro_sam/sam_annotator/_widgets.py
@@ -1,12 +1,10 @@
 from enum import Enum
 from pathlib import Path
+from typing import TYPE_CHECKING, Optional
 
-from magicgui import magicgui
-from napari.types import ImageData
-from superqt.utils import thread_worker
-
-from magicgui import magicgui
+from magicgui import magic_factory
 from magicgui.tqdm import tqdm
+from superqt.utils import thread_worker
 
 from micro_sam.util import (
     ImageEmbeddings,
@@ -16,30 +14,43 @@ from micro_sam.util import (
     _DEFAULT_MODEL,
 )
 
+if TYPE_CHECKING:
+    import napari
 
 Model = Enum("Model", _MODEL_URLS)
 
 
-@magicgui(call_button="Compute image embedding")
+@magic_factory(call_button="Compute image embeddings",
+               device = {"choices": ["auto", 'cuda', 'mps', 'cpu']})
 def embedding_widget(
-        image: ImageData,
-        model: Model = Model.__getitem__(_DEFAULT_MODEL),
-        save_path: Path | str | None = None,
-    ) -> ImageEmbeddings:
+    image: "napari.layers.Image",
+    model: Model = Model.__getitem__(_DEFAULT_MODEL),
+    device = "auto",
+    save_path: Optional[Path] = None,  # where embeddings for this image are cached (optional)
+    custom_model: Optional[str] = None,  # A filepath or URL to custom model weights.
+) -> ImageEmbeddings:
     """Image embedding widget."""
+    # for access to the predictor and the image embeddings in the widgets
+    global PREDICTOR, IMAGE_EMBEDDINGS
+    # Initialize the model
     PREDICTOR = get_sam_model(model_type=model.name)
+
+    # Get image dimensions
+    if not image.rgb:
+        ndim = image.data.ndim
+    else:
+        # assumes RGB channels are the last dimension
+        ndim = image.data.ndim
+
+    # Compute the embeddings for the image data
     with tqdm() as pbar:
         @thread_worker(connect={"finished": lambda: pbar.progressbar.hide()})
-        def _compute_image_embedding(image, model, save_path):
-            image_embeddings = precompute_image_embeddings(
+        def _compute_image_embedding(PREDICTOR, image, save_path, ndim=None):
+            IMAGE_EMBEDDINGS = precompute_image_embeddings(
                 predictor = PREDICTOR,
                 input_ = image,
                 save_path = str(save_path),
+                ndim=ndim,
             )
-            return image_embeddings
 
-        print("Computing image embedding...")
-        image_embeddings = _compute_image_embedding(image, model, save_path)
-        print("Finished image embedding computation.")
-
-    return image_embeddings
+        _compute_image_embedding(PREDICTOR, image.data, save_path, ndim=ndim)
