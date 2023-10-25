676b7ed067fd7118e4bd3a8fe3501a386f0b0e28
anwai98 <anwai.archit@gmail.com>
2023-10-25 13:33:09 +0200

    Refactor histopatho

--- finetuning/generalists/training/histopathology/train_histopathology_generalist.py
+++ finetuning/generalists/training/histopathology/train_histopathology_generalist.py
@@ -1,62 +1,50 @@
 import os
+import argparse
+
+import torch
+from torch_em.loss import DiceLoss
 
 import micro_sam.training as sam_training
-import torch
-import torch_em
-
-import torch.utils.data as data_util
-from torch_em.data.datasets import get_lizard_dataset
-from torch_em.data.sampler import MinInstanceSampler
 from micro_sam.util import export_custom_sam_model
 
-
-# TODO use other datasets than lizard
-def get_dataloaders(patch_shape, data_path):
-    label_transform = torch_em.transform.label.label_consecutive  # to ensure consecutive IDs
-    sampler = MinInstanceSampler(min_num_instances=5)
-    dataset = get_lizard_dataset(
-        path=data_path, download=True, patch_shape=patch_shape, label_transform=label_transform,
-        sampler=sampler,
-    )
-    train_ds, val_ds = data_util.random_split(dataset, [0.9, 0.1])
-    train_loader = torch_em.get_data_loader(train_ds, batch_size=1)
-    val_loader = torch_em.get_data_loader(val_ds, batch_size=1)
-    return train_loader, val_loader
+from obtain_hp_datasets import get_generalist_hp_loaders
 
 
-def finetune_histopatho(input_path, export_path, model_type="vit_h", iterations=int(2e4), save_root=None):
-    """Example code for finetuning SAM on LiveCELL"""
+def finetune_hp_generalist(args):
+    """Example code for finetuning SAM on multiple histopathology datasets"""
+    # override this (below) if you have some more complex set-up and need to specify the exact gpu
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
     # training settings:
+    model_type = args.model_type
     checkpoint_path = None  # override this to start training from a custom checkpoint
-    device = "cuda"  # override this if you have some more complex set-up and need to specify the exact gpu
     patch_shape = (512, 512)  # the patch shape for training
-    n_objects_per_batch = 50  # this is the number of objects per batch that will be sampled
-
-    train_loader, val_loader = get_dataloaders(patch_shape=patch_shape, data_path=input_path)
+    n_objects_per_batch = 25  # this is the number of objects per batch that will be sampled
+    freeze_parts = None  # override this to freeze one or more of these backbones
 
     # get the trainable segment anything model
-    model = sam_training.get_trainable_sam_model(model_type, checkpoint_path, device=device)
+    model = sam_training.get_trainable_sam_model(model_type, checkpoint_path, freeze_parts)
 
     # all the stuff we need for training
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", factor=0.9, patience=10, verbose=True)
+    train_loader, val_loader = get_generalist_hp_loaders(patch_shape=patch_shape, data_path=args.input_path)
 
     # this class creates all the training data for a batch (inputs, prompts and labels)
     convert_inputs = sam_training.ConvertToSamInputs()
 
-    checkpoint_name = "sam-histopatho-v1"
+    checkpoint_name = "generalist-hp-sam"
     # the trainer which performs training and validation (implemented using "torch_em")
     trainer = sam_training.SamTrainer(
         name=checkpoint_name,
-        save_root=save_root,
+        save_root=args.save_root,
         train_loader=train_loader,
         val_loader=val_loader,
         model=model,
         optimizer=optimizer,
         # currently we compute loss batch-wise, else we pass channelwise True
-        loss=torch_em.loss.DiceLoss(channelwise=False),
-        metric=torch_em.loss.DiceLoss(),
+        loss=DiceLoss(channelwise=False),
+        metric=DiceLoss(),
         device=device,
         lr_scheduler=scheduler,
         logger=sam_training.SamLogger,
@@ -67,22 +55,42 @@ def finetune_histopatho(input_path, export_path, model_type="vit_h", iterations=
         n_sub_iteration=8,
         compile_model=False
     )
-    trainer.fit(iterations)
-    if export_path is not None:
+    trainer.fit(iterations=args.iterations)
+    if args.export_path is not None:
         checkpoint_path = os.path.join(
-            "" if save_root is None else save_root, "checkpoints", checkpoint_name, "best.pt"
+            "" if args.save_root is None else args.save_root, "checkpoints", checkpoint_name, "best.pt"
         )
         export_custom_sam_model(
             checkpoint_path=checkpoint_path,
             model_type=model_type,
-            save_path=export_path,
+            save_path=args.export_path,
         )
 
 
 def main():
-    input_path = "/scratch-grete/projects/nim00007/data/lizard"
-    export_path = "./sam-vith-histopatho-v1.pth"
-    finetune_histopatho(input_path, export_path)
+    parser = argparse.ArgumentParser(description="Finetune Segment Anything for the Histopathology datasets.")
+    parser.add_argument(
+        "--input_path", "-i", default="/scratch/usr/nimanwai/data/",
+        help="The filepath to all the respective hp datasets. If the data does not exist yet it will be downloaded"
+    )
+    parser.add_argument(
+        "--model_type", "-m", default="vit_b",
+        help="The model type to use for fine-tuning. Either vit_h, vit_b or vit_l."
+    )
+    parser.add_argument(
+        "--save_root", "-s",
+        help="Where to save the checkpoint and logs. By default they will be saved where this script is run from."
+    )
+    parser.add_argument(
+        "--iterations", type=int, default=int(1e5),
+        help="For how many iterations should the model be trained? By default 100k."
+    )
+    parser.add_argument(
+        "--export_path", "-e",
+        help="Where to export the finetuned model to. The exported model can be used in the annotation tools."
+    )
+    args = parser.parse_args()
+    finetune_hp_generalist(args)
 
 
 if __name__ == "__main__":
