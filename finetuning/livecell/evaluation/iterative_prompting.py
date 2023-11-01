0f9f9d11e418e2459b623db4a6471beefdf31c8f
anwai98 <anwai.archit@gmail.com>
2023-11-01 13:10:07 +0100

    Refactor iterative prompting inference and evaluation script

--- finetuning/livecell/evaluation/iterative_prompting.py
+++ finetuning/livecell/evaluation/iterative_prompting.py
@@ -7,24 +7,30 @@ from micro_sam.evaluation.inference import run_inference_with_iterative_promptin
 from util import get_checkpoint, get_paths
 
 LIVECELL_GT_ROOT = "/scratch-grete/projects/nim00007/data/LiveCELL/annotations_corrected/livecell_test_images"
-# TODO update to make fit other models
-PREDICTION_ROOT = "./pred_interactive_prompting"
+PREDICTION_ROOT = "/scratch-grete/projects/nim00007/sam/iterative_evaluation"
 
 
-def run_interactive_prompting(use_get_checkpoint=False):
-    prediction_root = PREDICTION_ROOT
+def get_prediction_root(use_boxes, model_type, root_dir=PREDICTION_ROOT):
+    if use_boxes:
+        prediction_root = os.path.join(root_dir, model_type, "start_with_box")
+    else:
+        prediction_root = os.path.join(root_dir, model_type, "start_with_point")
 
-    checkpoint, model_type = get_checkpoint("vit_b")
+    return prediction_root
+
+
+def run_interactive_prompting(use_boxes, model_name, get_default_checkpoint_paths, prediction_root):
+    checkpoint, model_type = get_checkpoint(model_name)
     image_paths, gt_paths = get_paths()
 
     run_inference_with_iterative_prompting(
         image_paths=image_paths,
         gt_paths=gt_paths,
         prediction_root=prediction_root,
-        use_boxes=False,
+        use_boxes=use_boxes,
         batch_size=16,
-        checkpoint_path=checkpoint if use_get_checkpoint else None,
-        model_type=model_type if use_get_checkpoint else "vit_b"
+        checkpoint_path=checkpoint if get_default_checkpoint_paths else None,
+        model_type=model_type
     )
 
 
@@ -38,8 +44,9 @@ def get_pg_paths(pred_folder):
     return pred_paths, gt_paths
 
 
-def evaluate_interactive_prompting():
-    prediction_root = PREDICTION_ROOT
+def evaluate_interactive_prompting(prediction_root):
+    assert os.path.exists(prediction_root), prediction_root
+
     prediction_folders = sorted(glob(os.path.join(prediction_root, "iteration*")))
     for pred_folder in prediction_folders:
         print("Evaluating", pred_folder)
@@ -49,8 +56,15 @@ def evaluate_interactive_prompting():
 
 
 def main():
-    run_interactive_prompting()
-    evaluate_interactive_prompting()
+    use_boxes = True  # overwrite when you want the first iterations' input prompt to start as a box
+    model_name = "vit_b"  # overwrite to specify the choice of vanilla / finetuned models
+    get_default_checkpoint_paths = False  # use the default checkpoint paths or fetch from `~/.sam_models`
+
+    # add the root prediction path where you would like to save the iterative prompting results
+    prediction_root = get_prediction_root(use_boxes, model_name)
+
+    run_interactive_prompting(use_boxes, model_name, get_default_checkpoint_paths, prediction_root)
+    # evaluate_interactive_prompting(prediction_root)
 
 
 if __name__ == "__main__":
