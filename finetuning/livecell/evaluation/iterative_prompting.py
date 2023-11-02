0383d6554d22dde0c1f8174161a63d8a55c1d8b1
anwai98 <anwai.archit@gmail.com>
2023-11-02 22:37:08 +0100

    Fix iterative prompting - initializing predictor

--- finetuning/livecell/evaluation/iterative_prompting.py
+++ finetuning/livecell/evaluation/iterative_prompting.py
@@ -5,34 +5,27 @@ from glob import glob
 from micro_sam.evaluation import inference
 from micro_sam.evaluation.evaluation import run_evaluation
 
-from util import get_checkpoint, get_paths
+from util import get_paths, get_checkpoint
 
 LIVECELL_GT_ROOT = "/scratch/projects/nim00007/data/LiveCELL/annotations_corrected/livecell_test_images"
 PREDICTION_ROOT = "/scratch/projects/nim00007/sam/iterative_evaluation"
 
 
-def get_prediction_root(start_with_box_prompt, model_type, root_dir=PREDICTION_ROOT):
+def get_prediction_root(start_with_box_prompt, model_description, root_dir=PREDICTION_ROOT):
     if start_with_box_prompt:
-        prediction_root = os.path.join(root_dir, model_type, "start_with_box")
+        prediction_root = os.path.join(root_dir, model_description, "start_with_box")
     else:
-        prediction_root = os.path.join(root_dir, model_type, "start_with_point")
+        prediction_root = os.path.join(root_dir, model_description, "start_with_point")
 
     return prediction_root
 
 
-def run_interactive_prompting(start_with_box_prompt, model_name, prediction_root, checkpoint=None):
-    if checkpoint is None:
-        checkpoint, model_type = get_checkpoint(model_name)
-    else:
-        model_type = model_name
+def run_interactive_prompting(predictor, start_with_box_prompt, model_description, prediction_root):
+    # we organize all the folders with data from this experiment below
+    embedding_folder = os.path.join(PREDICTION_ROOT, model_description, "embeddings")
+    os.makedirs(embedding_folder, exist_ok=True)
 
     image_paths, gt_paths = get_paths()
-
-    predictor = inference.get_predictor(checkpoint, model_type)
-
-    # we organize all the folders with data from this experiment below
-    embedding_folder = os.path.join(PREDICTION_ROOT, model_type, "embeddings")
-    os.makedirs(embedding_folder, exist_ok=True)
 
     inference.run_inference_with_iterative_prompting(
         predictor=predictor,
@@ -54,10 +47,10 @@ def get_pg_paths(pred_folder):
     return pred_paths, gt_paths
 
 
-def evaluate_interactive_prompting(prediction_root, start_with_box_prompt, model_name):
+def evaluate_interactive_prompting(prediction_root, start_with_box_prompt, model_description):
     assert os.path.exists(prediction_root), prediction_root
 
-    csv_save_dir = f"./iterative_prompting_results/{model_name}"
+    csv_save_dir = f"./iterative_prompting_results/{model_description}"
     os.makedirs(csv_save_dir, exist_ok=True)
     csv_path = os.path.join(csv_save_dir, "start_with_box.csv" if start_with_box_prompt else "start_with_point.csv")
     if os.path.exists(csv_path):
@@ -78,15 +71,19 @@ def evaluate_interactive_prompting(prediction_root, start_with_box_prompt, model
 
 
 def main():
-    start_with_box_prompt = True  # overwrite when you want first iters' prompt to start as box instead of single point
-    model_name = "vit_h"  # overwrite to specify the choice of vanilla / finetuned models
-    checkpoint = None  # overwrite to pass your expected model's checkpoint path
+    start_with_box_prompt = False  # overwrite when you want first iters' prompt to start as box instead of single point
+    model_description = "vit_h_generalist"  # overwrite to specify the choice of vanilla / finetuned models
 
     # add the root prediction path where you would like to save the iterative prompting results
-    prediction_root = get_prediction_root(start_with_box_prompt, model_name)
+    prediction_root = get_prediction_root(start_with_box_prompt, model_description)
 
-    run_interactive_prompting(start_with_box_prompt, model_name, prediction_root, checkpoint=checkpoint)
-    evaluate_interactive_prompting(prediction_root, start_with_box_prompt, model_name)
+    # get the model checkpoints and desired model name to initialize the predictor
+    checkpoint, model_type = get_checkpoint(model_description)
+    # get the predictor to perform inference
+    predictor = inference.get_predictor(checkpoint, model_type)
+
+    run_interactive_prompting(predictor, start_with_box_prompt, model_description, prediction_root)
+    evaluate_interactive_prompting(prediction_root, start_with_box_prompt, model_description)
 
 
 if __name__ == "__main__":
