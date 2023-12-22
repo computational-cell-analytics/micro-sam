a2b33b8f78dc2c41f707b1510426e40bb1c55d9e
anwai98 <anwai.archit@gmail.com>
2023-12-22 21:48:36 +0100

    Update LM dataloaders

--- finetuning/generalists/training/light_microscopy/obtain_lm_datasets.py
+++ finetuning/generalists/training/light_microscopy/obtain_lm_datasets.py
@@ -7,49 +7,65 @@ import torch_em
 import torch_em.data.datasets as datasets
 from torch_em.transform.label import label_consecutive
 from torch_em.data import MinInstanceSampler, ConcatDataset
-from torch_em.transform.raw import standardize, normalize_percentile
+from torch_em.transform.raw import normalize_percentile, normalize
 
 
 def neurips_raw_trafo(raw):
     raw = datasets.neurips_cell_seg.to_rgb(raw)  # ensures 3 channels for the neurips data
     raw = normalize_percentile(raw)
     raw = np.mean(raw, axis=0)
-    raw = standardize(raw)
+    raw = normalize(raw)
+    raw = raw * 255
     return raw
 
 
-def tissuenet_raw_trafo(raw, desired_shape=(512, 512)):
-    raw = normalize_percentile(raw, axis=(1, 2))
-    raw = np.mean(raw, axis=0)
-    raw = standardize(raw)
-
-    tmp_ddim = (desired_shape[0] - raw.shape[0], desired_shape[1] - raw.shape[1])
-    ddim = (tmp_ddim[0] / 2, tmp_ddim[1] / 2)
-    raw = np.pad(raw, pad_width=((ceil(ddim[0]), floor(ddim[0])), (ceil(ddim[1]), floor(ddim[1]))), mode="reflect")
-    assert raw.shape == desired_shape
+def deepbacs_raw_trafo(raw):
+    raw = normalize(raw)
+    raw = raw * 255
     return raw
 
 
-def tissuenet_label_trafo(labels, desired_shape=(512, 512)):
-    labels = label_consecutive(labels)
+class ResizeRawTrafo:
+    def __init__(self, desired_shape, do_rescaling=True, padding="constant"):
+        self.desired_shape = desired_shape
+        self.padding = padding
+        self.do_rescaling = do_rescaling
 
-    tmp_ddim = (desired_shape[0] - labels.shape[0], desired_shape[1] - labels.shape[1])
-    ddim = (tmp_ddim[0] / 2, tmp_ddim[1] / 2)
-    labels = np.pad(
-        labels,
-        pad_width=((ceil(ddim[0]), floor(ddim[0])), (ceil(ddim[1]), floor(ddim[1]))),
-        mode="constant"
-    )
-    assert labels.shape == desired_shape
-    return labels
+    def __call__(self, raw):
+        if self.do_rescaling:
+            raw = normalize_percentile(raw, axis=(1, 2))
+            raw = np.mean(raw, axis=0)
+            raw = normalize(raw)
+            raw = raw * 255
+
+        tmp_ddim = (self.desired_shape[0] - raw.shape[0], self.desired_shape[1] - raw.shape[1])
+        ddim = (tmp_ddim[0] / 2, tmp_ddim[1] / 2)
+        raw = np.pad(
+            raw,
+            pad_width=((ceil(ddim[0]), floor(ddim[0])), (ceil(ddim[1]), floor(ddim[1]))),
+            mode=self.padding
+        )
+        assert raw.shape == self.desired_shape
+        return raw
 
 
-def raw_padding_trafo(raw, desired_shape=(512, 512)):
-    tmp_ddim = (desired_shape[0] - raw.shape[0], desired_shape[1] - raw.shape[1])
-    ddim = (tmp_ddim[0] / 2, tmp_ddim[1] / 2)
-    raw = np.pad(raw, pad_width=((ceil(ddim[0]), floor(ddim[0])), (ceil(ddim[1]), floor(ddim[1]))), mode="reflect")
-    assert raw.shape == desired_shape
-    return raw
+class ResizeLabelTrafo:
+    def __init__(self, desired_shape, padding="constant"):
+        self.desired_shape = desired_shape
+        self.padding = padding
+
+    def __call__(self, labels):
+        labels = label_consecutive(labels)
+
+        tmp_ddim = (self.desired_shape[0] - labels.shape[0], self.desired_shape[1] - labels.shape[1])
+        ddim = (tmp_ddim[0] / 2, tmp_ddim[1] / 2)
+        labels = np.pad(
+            labels,
+            pad_width=((ceil(ddim[0]), floor(ddim[0])), (ceil(ddim[1]), floor(ddim[1]))),
+            mode=self.padding
+        )
+        assert labels.shape == self.desired_shape
+        return labels
 
 
 def get_concat_lm_datasets(input_path, patch_shape, split_choice):
@@ -62,8 +78,8 @@ def get_concat_lm_datasets(input_path, patch_shape, split_choice):
     generalist_dataset = ConcatDataset(
         datasets.get_tissuenet_dataset(
             path=os.path.join(input_path, "tissuenet"), split=split_choice, download=True,
-            patch_shape=patch_shape if split_choice == "train" else (256, 256), raw_channel="rgb",
-            label_channel="cell", raw_transform=tissuenet_raw_trafo, label_transform=tissuenet_label_trafo,
+            patch_shape=patch_shape, raw_channel="rgb", label_channel="cell",
+            raw_transform=ResizeRawTrafo(patch_shape), label_transform=ResizeLabelTrafo(patch_shape),
             sampler=sampler, label_dtype=label_dtype, n_samples=1000 if split_choice == "train" else 100
         ),
         datasets.get_livecell_dataset(
@@ -74,7 +90,7 @@ def get_concat_lm_datasets(input_path, patch_shape, split_choice):
         datasets.get_deepbacs_dataset(
             path=os.path.join(input_path, "deepbacs"), split=split_choice if split_choice == "train" else "test",
             patch_shape=patch_shape, label_transform=label_transform, sampler=sampler, label_dtype=label_dtype,
-            download=True
+            download=True, raw_transform=deepbacs_raw_trafo
         ),
         datasets.get_neurips_cellseg_supervised_dataset(
             root=os.path.join(input_path, "neurips-cell-seg"), split=split_choice, patch_shape=patch_shape,
@@ -87,8 +103,8 @@ def get_concat_lm_datasets(input_path, patch_shape, split_choice):
         ),
         datasets.get_plantseg_dataset(
             path=os.path.join(input_path, "plantseg"), name="root", sampler=MinInstanceSampler(min_num_instances=10),
-            label_transform=tissuenet_label_trafo, ndim=2, split=split_choice, label_dtype=label_dtype,
-            raw_transform=raw_padding_trafo, patch_shape=(1, patch_shape[0], patch_shape[1]), download=True,
+            label_transform=ResizeLabelTrafo(patch_shape), ndim=2, split=split_choice, label_dtype=label_dtype,
+            raw_transform=ResizeRawTrafo(patch_shape, do_rescaling=False), patch_shape=(1, patch_shape[0], patch_shape[1]), download=True,
             n_samples=1000 if split_choice == "train" else 100
         )
 
