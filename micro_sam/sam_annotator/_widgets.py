a644dd73dd00aff99ee0d762179819e399c611ca
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2024-01-04 21:41:57 +0100

    Start refactor for plugin

--- micro_sam/sam_annotator/_widgets.py
+++ micro_sam/sam_annotator/_widgets.py
@@ -17,6 +17,7 @@ from micro_sam.util import (
     _available_devices,
     get_cache_directory,
 )
+from . import util as vutil
 
 if TYPE_CHECKING:
     import napari
@@ -26,7 +27,7 @@ if TYPE_CHECKING:
     pbar={'visible': False, 'max': 0, 'value': 0, 'label': 'working...'},
     call_button="Compute image embeddings",
     save_path={"mode": "d"},  # choose a directory
-    )
+)
 def embedding_widget(
     pbar: widgets.ProgressBar,
     image: "napari.layers.Image",
@@ -94,3 +95,34 @@ def settings_widget(
     """Update micro-sam settings."""
     os.environ["MICROSAM_CACHEDIR"] = str(cache_directory)
     print(f"micro-sam cache directory set to: {cache_directory}")
+
+
+# TODO: don't pass the viewer but just the appropriate layers
+# TODO: fail more gracefully if image embeddings have not been initialized
+@magic_factory(call_button="Segment Object [S]")
+def segment_widget(v: "napari.viewer.Viewer", box_extension: float = 0.1) -> None:
+    shape = v.layers["current_object"].data.shape
+
+    # get the current box and point prompts
+    boxes, masks = vutil.shape_layer_to_prompts(v.layers["prompts"], shape)
+    points, labels = vutil.point_layer_to_prompts(v.layers["point_prompts"], with_stop_annotation=False)
+
+    predictor = AnnotatorState().predictor
+    image_embeddings = AnnotatorState().image_embeddings
+    if image_embeddings["original_size"] is None:  # tiled prediction
+        seg = vutil.prompt_segmentation(
+            predictor, points, labels, boxes, masks, shape, image_embeddings=image_embeddings,
+            multiple_box_prompts=True, box_extension=box_extension,
+        )
+    else:  # normal prediction and we have set the precomputed embeddings already
+        seg = vutil.prompt_segmentation(
+            predictor, points, labels, boxes, masks, shape, multiple_box_prompts=True, box_extension=box_extension,
+        )
+
+    # no prompts were given or prompts were invalid, skip segmentation
+    if seg is None:
+        print("You either haven't provided any prompts or invalid prompts. The segmentation will be skipped.")
+        return
+
+    v.layers["current_object"].data = seg
+    v.layers["current_object"].refresh()
