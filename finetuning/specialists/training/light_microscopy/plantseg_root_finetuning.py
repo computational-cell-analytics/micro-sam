e2e04a9e6ce1715a01b95b8bd4756f959b7f6ffb
Anwai Archit <52396323+anwai98@users.noreply.github.com>
2024-01-05 11:22:08 +0100

    Update LM Generalists (#292)
    Update training for LM datasets

--- finetuning/specialists/training/light_microscopy/plantseg_root_finetuning.py
+++ finetuning/specialists/training/light_microscopy/plantseg_root_finetuning.py
@@ -4,16 +4,47 @@ import argparse
 import torch
 
 from torch_em.model import UNETR
+from torch_em.data import MinInstanceSampler
 from torch_em.loss import DiceBasedDistanceLoss
+from torch_em.data.datasets import get_plantseg_loader
 
 import micro_sam.training as sam_training
 from micro_sam.util import export_custom_sam_model
+from micro_sam.training.util import ResizeLabelTrafo, ResizeRawTrafo
 
-from obtain_lm_datasets import get_generalist_lm_loaders
+
+def get_dataloaders(patch_shape, data_path):
+    """This returns the plantseg data loaders implemented in torch_em:
+    https://github.com/constantinpape/torch-em/blob/main/torch_em/data/datasets/plantseg.py
+    It will automatically download the plantseg (root) data.
+
+    Note: to replace this with another data loader you need to return a torch data loader
+    that retuns `x, y` tensors, where `x` is the image data and `y` are the labels.
+    The labels have to be in a label mask instance segmentation format.
+    I.e. a tensor of the same spatial shape as `x`, with each object mask having its own ID.
+    Important: the ID 0 is reseved for background, and the IDs must be consecutive
+    """
+    raw_transform = ResizeRawTrafo(patch_shape, do_rescaling=False)
+    label_transform = ResizeLabelTrafo(patch_shape)
+    sampler = MinInstanceSampler(min_num_instances=10)
+    label_dtype = torch.float32
+
+    train_loader = get_plantseg_loader(
+        path=data_path, name="root", split="train", patch_shape=(1, *patch_shape), batch_size=2,
+        download=True, ndim=2, sampler=sampler, raw_transform=raw_transform, label_transform=label_transform,
+        num_workers=16, shuffle=True, label_dtype=label_dtype, n_samples=5000  # training w. ~25% of the total train-set
+    )
+    val_loader = get_plantseg_loader(
+        path=data_path, name="root", split="val", patch_shape=(1, *patch_shape), batch_size=1,
+        download=True, ndim=2, sampler=sampler, raw_transform=raw_transform, label_transform=label_transform,
+        num_workers=16, shuffle=True, label_dtype=label_dtype
+    )
+
+    return train_loader, val_loader
 
 
-def finetune_lm_generalist(args):
-    """Code for finetuning SAM on multiple light microscopy datasets"""
+def finetune_plantseg_root(args):
+    """Code for finetuning SAM on PlantSeg (root)"""
     # override this (below) if you have some more complex set-up and need to specify the exact gpu
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -22,7 +53,7 @@ def finetune_lm_generalist(args):
     checkpoint_path = None  # override this to start training from a custom checkpoint
     patch_shape = (512, 512)  # the patch shape for training
     n_objects_per_batch = 25  # this is the number of objects per batch that will be sampled
-    freeze_parts = None  # override this to freeze one or more of these backbones
+    freeze_parts = args.freeze  # override this to freeze different parts of the model
 
     # get the trainable segment anything model
     model = sam_training.get_trainable_sam_model(
@@ -54,12 +85,14 @@ def finetune_lm_generalist(args):
     # all the stuff we need for training
     optimizer = torch.optim.Adam(joint_model_params, lr=1e-5)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", factor=0.9, patience=10, verbose=True)
-    train_loader, val_loader = get_generalist_lm_loaders(input_path=args.input_path, patch_shape=patch_shape)
+    train_loader, val_loader = get_dataloaders(patch_shape=patch_shape, data_path=args.input_path)
 
     # this class creates all the training data for a batch (inputs, prompts and labels)
-    convert_inputs = sam_training.ConvertToSamInputs(transform=model.transform, box_distortion_factor=0.025)
+    convert_inputs = sam_training.ConvertToSamInputs(
+        transform=model.transform, box_distortion_factor=0.025
+    )
 
-    checkpoint_name = f"{args.model_type}/lm_generalist_sam"
+    checkpoint_name = f"{args.model_type}/plantseg_root_sam"
 
     # the trainer which performs the joint training and validation (implemented using "torch_em")
     trainer = sam_training.JointSamTrainer(
@@ -96,10 +129,10 @@ def finetune_lm_generalist(args):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Finetune Segment Anything for the LM datasets.")
+    parser = argparse.ArgumentParser(description="Finetune Segment Anything for the PlantSeg dataset.")
     parser.add_argument(
-        "--input_path", "-i", default="/scratch/usr/nimanwai/data/",
-        help="The filepath to all the respective LM datasets. If the data does not exist yet it will be downloaded"
+        "--input_path", "-i", default="/scratch/usr/nimanwai/data/plantseg/",
+        help="The filepath to the PlantSeg (root) data. If the data does not exist yet it will be downloaded."
     )
     parser.add_argument(
         "--model_type", "-m", default="vit_b",
@@ -107,10 +140,10 @@ def main():
     )
     parser.add_argument(
         "--save_root", "-s",
-        help="Where to save the checkpoint and logs. By default they will be saved where this script is run from."
+        help="Where to save the checkpoint and logs. By default they will be saved where this script is run."
     )
     parser.add_argument(
-        "--iterations", type=int, default=int(25e4),
+        "--iterations", type=int, default=int(1e5),
         help="For how many iterations should the model be trained? By default 100k."
     )
     parser.add_argument(
@@ -118,11 +151,15 @@ def main():
         help="Where to export the finetuned model to. The exported model can be used in the annotation tools."
     )
     parser.add_argument(
+        "--freeze", type=str, nargs="+", default=None,
+        help="Which parts of the model to freeze for finetuning."
+    )
+    parser.add_argument(
         "--save_every_kth_epoch", type=int, default=None,
         help="To save every kth epoch while fine-tuning. Expects an integer value."
     )
     args = parser.parse_args()
-    finetune_lm_generalist(args)
+    finetune_plantseg_root(args)
 
 
 if __name__ == "__main__":
