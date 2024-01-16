881ac7efa043591b86478b6d6c8cfac312131ab7
anwai98 <anwai.archit@gmail.com>
2024-01-16 15:53:22 +0100

    Add amg

---
+++ finetuning/generalists/evaluation/evaluate_amg.py
@@ -0,0 +1,48 @@
+import os
+import argparse
+
+from micro_sam.evaluation.evaluation import run_evaluation
+
+from util import get_pred_paths, run_amg, get_paths
+
+
+def run_em_amg(dataset_name, model_type, checkpoint, experiment_folder):
+    val_image_paths, val_gt_paths = get_paths(dataset_name, split="val")
+    test_image_paths, _ = get_paths(dataset_name, split="test")
+    prediction_folder = run_amg(
+        checkpoint,
+        model_type,
+        experiment_folder,
+        val_image_paths,
+        val_gt_paths,
+        test_image_paths
+    )
+    return prediction_folder
+
+
+def eval_amg(dataset_name, prediction_folder, experiment_folder):
+    print("Evaluating", prediction_folder)
+    _, gt_paths = get_paths(dataset_name)
+    pred_paths = get_pred_paths(prediction_folder)
+    save_path = os.path.join(experiment_folder, "results", "amg.csv")
+    res = run_evaluation(gt_paths, pred_paths, save_path=save_path)
+    print(res)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-m", "--model", type=str, required=True,
+        help="Provide the model type to initialize the predictor"
+    )
+    parser.add_argument("-c", "--checkpoint", type=str, required=True)
+    parser.add_argument("-e", "--experiment_folder", type=str, required=True)
+    parser.add_argument("-d", "--dataset", type=str, required=True)
+    args = parser.parse_args()
+
+    prediction_folder = run_em_amg(args.dataset, args.model, args.checkpoint, args.experiment_folder)
+    eval_amg(args.dataset, prediction_folder, args.experiment_folder)
+
+
+if __name__ == "__main__":
+    main()
--- finetuning/generalists/evaluation/util.py
+++ finetuning/generalists/evaluation/util.py
@@ -1,11 +1,17 @@
 import os
 from glob import glob
+from typing import Union, List, Optional
 
 from micro_sam.evaluation import get_predictor
+from micro_sam import instance_segmentation, inference
+from micro_sam.instance_segmentation import AutomaticMaskGenerator
 
 
 DATASETS = {
-    "lucchi": [None, None]
+    "lucchi": {
+        "val": [None, None],
+        "test": [None, None]
+    }
 }
 
 
@@ -14,9 +20,9 @@ def get_model(model_type, ckpt):
     return predictor
 
 
-def get_paths(dataset_name):
+def get_paths(dataset_name, split="test"):
     assert dataset_name in DATASETS
-    image_dir, gt_dir = DATASETS[dataset_name]
+    image_dir, gt_dir = DATASETS[dataset_name][split]
     image_paths = glob(os.path.join(image_dir, "*.tif"))
     gt_paths = glob(os.path.join(gt_dir, "*.tif"))
     return image_paths, gt_paths
@@ -32,3 +38,46 @@ def download_em_dataset(dataset_name):
 
     # TODO: downloading the dataset using the dataloaders (for all splits - especially val and test split)
     raise NotImplementedError
+
+
+#
+# AMG FUNCTION (will move it to `micro_sam.evaluation.util` after testing)
+#
+
+
+def run_amg(
+    checkpoint: Union[str, os.PathLike],
+    model_type: str,
+    experiment_folder: Union[str, os.PathLike],
+    val_image_paths: List[Union[str, os.PathLike]],
+    val_gt_paths: List[Union[str, os.PathLike]],
+    test_image_paths: List[Union[str, os.PathLike]],
+    iou_thresh_values: Optional[List[float]] = None,
+    stability_score_values: Optional[List[float]] = None,
+) -> str:
+    embedding_folder = os.path.join(experiment_folder, "embeddings")  # where the precomputed embeddings are saved
+    os.makedirs(embedding_folder, exist_ok=True)
+
+    predictor = inference.get_predictor(checkpoint, model_type)
+    amg = AutomaticMaskGenerator(predictor)
+    amg_prefix = "amg"
+
+    # where the predictions are saved
+    prediction_folder = os.path.join(experiment_folder, amg_prefix, "inference")
+    os.makedirs(prediction_folder, exist_ok=True)
+
+    # where the grid-search results are saved
+    gs_result_folder = os.path.join(experiment_folder, amg_prefix, "grid_search")
+    os.makedirs(gs_result_folder, exist_ok=True)
+
+    grid_search_values = instance_segmentation.default_grid_search_values_amg(
+        iou_thresh_values=iou_thresh_values,
+        stability_score_values=stability_score_values,
+    )
+
+    instance_segmentation.run_instance_segmentation_grid_search_and_inference(
+        amg, grid_search_values,
+        val_image_paths, val_gt_paths, test_image_paths,
+        embedding_folder, prediction_folder, gs_result_folder,
+    )
+    return prediction_folder
