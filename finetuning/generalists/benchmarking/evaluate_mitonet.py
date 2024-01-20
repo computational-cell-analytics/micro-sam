e11e29eb449b184574ce1eca1fc4ac0525967849
anwai98 <anwai.archit@gmail.com>
2024-01-20 21:00:25 +0100

    Update mitonet scripts

--- finetuning/generalists/benchmarking/evaluate_mitonet.py
+++ finetuning/generalists/benchmarking/evaluate_mitonet.py
@@ -10,10 +10,9 @@ def get_mitonet_predictions(volume) -> str:
     raise NotImplementedError("The volumes have been currently generated using `empanada-napari`")
 
 
-def evaluate_mitonet_predictions():
-    prediction_paths = sorted(glob(os.path.join(ROOT, "slices", "*.tif")))
-    gt_paths = sorted(glob("/scratch/projects/nim00007/sam/data/lucchi/slices/labels/lucchi_test_*"))
-    save_path = os.path.join(ROOT, "results", "mitonet.csv")
+def evaluate_mitonet_predictions(dataset_name, gt_paths):
+    prediction_paths = sorted(glob(os.path.join(ROOT, dataset_name, "slices", "*.tif")))
+    save_path = os.path.join(ROOT, dataset_name, "results", "mitonet.csv")
     res = run_evaluation(gt_paths, prediction_paths, save_path)
     print(res)
 
@@ -22,8 +21,17 @@ def main():
     # let's generate the predictions
     # prediction_folder = get_mitonet_predictions(volume)
 
+    dataset_name = "mitoem"
+
+    if dataset_name == "lucchi":
+        gt_paths = sorted(glob("/scratch/projects/nim00007/sam/data/lucchi/slices/labels/lucchi_test_*"))
+    elif dataset_name == "mitoem":
+        gt_paths = sorted(glob("/scratch/projects/nim00007/sam/data/mitoem/slices/*/test/labels/mitoem_*"))
+    else:
+        raise ValueError
+
     # evaluation of the instance segmentations from mitonet
-    evaluate_mitonet_predictions()
+    evaluate_mitonet_predictions(dataset_name, gt_paths)
 
 
 if __name__ == "__main__":
--- finetuning/generalists/benchmarking/process_mitonet_labels.py
+++ finetuning/generalists/benchmarking/process_mitonet_labels.py
@@ -1,28 +1,40 @@
 import os
+from glob import glob
 from tqdm import tqdm
+from pathlib import Path
 
 import imageio.v3 as imageio
 from skimage.segmentation import relabel_sequential
 
-ROOT = "/scratch-grete/projects/nim00007/sam/experiments/benchmarking/mitonet/lucchi/"
+ROOT = "/scratch-grete/projects/nim00007/sam/experiments/benchmarking/mitonet/"
 
 
-def make_slices_from_mitolab_predictions(labels_path, save_dir):
+def make_slices_from_mitolab_predictions(labels_path, save_dir, counter=0):
     labels = imageio.imread(labels_path)
-    for i, _slice in tqdm(enumerate(labels), total=labels.shape[0]):
+    label_id = Path(labels_path).stem
+
+    os.makedirs(save_dir, exist_ok=True)
+
+    for _slice in tqdm(labels, total=labels.shape[0]):
         relabeled_slice = relabel_sequential(_slice)[0]
-        save_path = os.path.join(save_dir, f"lucchi_test_{i+1:05}.tif")
+        save_path = os.path.join(save_dir, f"{label_id}_{counter+1:05}.tif")
         imageio.imwrite(save_path, relabeled_slice, compression="zlib")
+        counter += 1
 
 
 def main():
-    # we slice the label stack and save it here
-    os.makedirs(os.path.join(ROOT, "slices"), exist_ok=True)
-
+    # for lucchi
     make_slices_from_mitolab_predictions(
-        labels_path=os.path.join(ROOT, "lucchi_test_batch_segs_from_mitonet_default_params.tif"),
-        save_dir=os.path.join(ROOT, "slices")
+        labels_path=os.path.join(ROOT, "lucchi", "lucchi_test_batch_segs_from_mitonet_default_params.tif"),
+        save_dir=os.path.join(ROOT, "lucchi", "slices")
     )
+
+    # for mitoem
+    mitoem_seg_paths = glob(os.path.join(ROOT, "mitoem", "mitoem_*_test_batch_segs.tif"))
+    for seg_path in mitoem_seg_paths:
+        make_slices_from_mitolab_predictions(
+            labels_path=seg_path, save_dir=os.path.join(ROOT, "mitoem", "slices"), counter=5
+        )
 
 
 if __name__ == "__main__":
