a94d8dc78a6d5c233104be04d90497e710753b4b
anwai98 <anwai.archit@gmail.com>
2024-02-10 22:54:33 +0100

    Update cellpose for all lm datasets

---
+++ finetuning/evaluation/cellpose_baseline.py
@@ -0,0 +1,100 @@
+import os
+import pandas as pd
+from tqdm import tqdm
+
+import imageio.v3 as imageio
+
+from micro_sam.evaluation.evaluation import run_evaluation
+
+from util import get_paths, get_pred_paths
+
+
+EXPERIMENT_ROOT = "/scratch/projects/nim00007/sam/experiments/benchmarking/cellpose"
+
+LM_DATASETS = [
+    # in-domain (LM)
+    "tissuenet", "deepbacs", "plantseg/root", "livecell", "neurips-cell-seg",
+    # out-of-domain (LM)
+    "covid_if", "plantseg/ovules", "hpa", "lizard", "mouse-embryo", "ctc/hela_samples"
+]
+
+
+def load_cellpose_model(model_type):
+    from cellpose import models
+    device, gpu = models.assign_device(True, True)
+
+    if model_type in ["cyto", "cyto2", "nuclei"]:
+        model = models.Cellpose(gpu=gpu, model_type=model_type, device=device, net_avg=True)
+    elif model_type in ["livecell", "tissuenet"]:
+        model = models.CellposeModel(gpu=gpu, model_type=model_type, device=device)
+    else:
+        raise ValueError(model_type)
+
+    return model
+
+
+def run_cellpose_segmentation(dataset, model_type):
+    prediction_folder = os.path.join(EXPERIMENT_ROOT, dataset, model_type, "predictions")
+    os.makedirs(prediction_folder, exist_ok=True)
+
+    image_paths, _ = get_paths(dataset, split="test")
+    model = load_cellpose_model(model_type)
+
+    for path in tqdm(image_paths, desc=f"Segmenting {dataset} with cellpose ({model_type})"):
+        fname = os.path.basename(path)
+        out_path = os.path.join(prediction_folder, fname)
+        if os.path.exists(out_path):
+            continue
+        image = imageio.imread(path)
+        if image.ndim == 3:
+            assert image.shape[-1] == 3
+            image = image.mean(axis=-1)
+        assert image.ndim == 2
+        seg = model.eval(image, diameter=None, flow_threshold=None, channels=[0, 0])[0]
+        assert seg.shape == image.shape
+        imageio.imwrite(out_path, seg, compression=5)
+
+    return prediction_folder
+
+
+def evaluate_dataset(prediction_folder, dataset, model_type):
+    _, gt_paths = get_paths(dataset, split="test")
+    pred_paths = get_pred_paths(prediction_folder)
+    assert len(gt_paths) == len(pred_paths), f"{len(gt_paths)}, {len(pred_paths)}"
+    result_path = os.path.join(EXPERIMENT_ROOT, dataset, "results", f"cellpose-{model_type}.csv")
+    if os.path.exists(result_path):
+        print(pd.read_csv(result_path))
+        print(f"Results are already saved at {result_path}")
+        return result_path
+
+    results = run_evaluation(gt_paths, pred_paths, result_path)
+    print(results)
+    print(f"Results are saved at {result_path}")
+
+
+def run_cellpose_baseline(datasets, model_type):
+    if isinstance(datasets, str):
+        datasets = [datasets]
+
+    for dataset in datasets:
+        prediction_folder = run_cellpose_segmentation(dataset, model_type)
+        evaluate_dataset(prediction_folder, dataset, model_type)
+
+
+def main(args):
+    if args.dataset is None:
+        datasets = LM_DATASETS
+    else:
+        datasets = args.dataset
+        assert datasets in LM_DATASETS
+
+    run_cellpose_baseline(datasets, args.model_type)
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dataset", type=str, default=None)
+    parser.add_argument("-m", "--model_type", type=str, default="cyto")
+    args = parser.parse_args()
+    main(args)
