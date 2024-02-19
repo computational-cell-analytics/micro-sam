620969376ee8d0d54af96832b4212c7d2b44e71d
anwai98 <anwai.archit@gmail.com>
2024-02-19 16:24:12 +0100

    Update evaluation scripts - add vit_t and vit_l inference

--- finetuning/evaluation/util.py
+++ finetuning/evaluation/util.py
@@ -14,6 +14,7 @@ ROOT = "/scratch/projects/nim00007/sam/data/"
 EXPERIMENT_ROOT = "/scratch/projects/nim00007/sam/experiments/new_models"
 
 VANILLA_MODELS = {
+    "vit_t": "/scratch-grete/projects/nim00007/sam/models/new_models/vanilla/vit_t_mobile_sam.pth",
     "vit_b": "/scratch-grete/projects/nim00007/sam/models/new_models/vanilla/sam_vit_b_01ec64.pth",
     "vit_l": "/scratch-grete/projects/nim00007/sam/models/new_models/vanilla/sam_vit_l_0b3195.pth",
     "vit_h": "/scratch-grete/projects/nim00007/sam/models/new_models/vanilla/sam_vit_h_4b8939.pth"
