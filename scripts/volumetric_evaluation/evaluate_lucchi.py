d69ee00853cd30b5082250cfc6f0679ea15fea22
Anwai Archit <anwai.archit@gmail.com>
2024-02-27 14:38:09 +0100

    Update for considering custom checkpoints

--- scripts/volumetric_evaluation/evaluate_lucchi.py
+++ scripts/volumetric_evaluation/evaluate_lucchi.py
@@ -22,8 +22,8 @@ def get_raw_and_label_volumes(volume_path):
     return raw, label
 
 
-def segment_lucchi_from_slices():
-    predictor = util.get_sam_model(model_type="vit_b_em_organelles")
+def segment_lucchi_from_slices(model_type, checkpoint):
+    predictor = util.get_custom_sam_model(model_type=model_type, checkpoint_path=checkpoint)
     embedding_path = "./embeddings/"
 
     test_volume_path = os.path.join(ROOT, "lucchi_test.h5")
@@ -72,9 +72,17 @@ def segment_lucchi_from_slices():
         v.add_labels(this_seg)
         napari.run()
 
-def main():
-    segment_lucchi_from_slices()
+def main(args):
+    segment_lucchi_from_slices(model_type=args.model_type, checkpoint=args.checkpoint)
 
 
 if __name__ == "__main__":
-    main()
+    import warnings
+    warnings.filterwarnings("ignore")
+
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model_type", type=str, default="vit_b", help="Name of the image encoder")
+    parser.add_argument("-c", "--checkpoint", type=str, default=None, help="The custom checkpoint path.")
+    args = parser.parse_args()
+    main(args)
