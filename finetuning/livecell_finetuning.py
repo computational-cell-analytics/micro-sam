7085184d77b94066c225bace3400dda1968bfa04
Anwai Archit <52396323+anwai98@users.noreply.github.com>
2024-03-08 13:55:03 +0100

    Update livecell_finetuning.py

--- finetuning/livecell_finetuning.py
+++ finetuning/livecell_finetuning.py
@@ -142,8 +142,8 @@ def main():
         help="Where to save the checkpoint and logs. By default they will be saved where this script is run."
     )
     parser.add_argument(
-        "--iterations", type=int, default=int(25e4),
-        help="For how many iterations should the model be trained? By default 250k."
+        "--iterations", type=int, default=int(1e5),
+        help="For how many iterations should the model be trained? By default 100k."
     )
     parser.add_argument(
         "--export_path", "-e",
