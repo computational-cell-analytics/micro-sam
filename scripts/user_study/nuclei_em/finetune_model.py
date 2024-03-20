f58b5d8ca8ef89132befa51284f6ac7080a2c1e0
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2024-03-20 21:42:11 +0100

    Add scripts for EM user study

--- scripts/user_study/nuclei_em/finetune_model.py
+++ scripts/user_study/nuclei_em/finetune_model.py
@@ -9,15 +9,14 @@ from micro_sam.util import export_custom_sam_model
 
 
 def get_loader(split):
-    raw_path = "/scratch-emmy/projects/nim00007/fruit-fly-data/nucleus_segmentation/micro_sam/data_block_58.h5"
-    label_path = "/scratch-emmy/projects/nim00007/fruit-fly-data/nucleus_segmentation/micro_sam/data_block_58_segmentation.tif"
+    data_path = "/scratch-grete/projects/nim00007/sam/user_study/nuclei_em/v2/train_data.h5"
 
     batch_size = 2
     if split == "train":
-        roi = np.s_[:50, :, :]
+        roi = np.s_[:24, :, :]
         n_samples = batch_size * 100
     else:
-        roi = np.s_[50:, :, :]
+        roi = np.s_[24:, :, :]
         n_samples = batch_size * 4
 
     label_transform = PerObjectDistanceTransform(
@@ -27,8 +26,8 @@ def get_loader(split):
 
     patch_shape = (1, 512, 512)
     loader = torch_em.default_segmentation_loader(
-        raw_paths=raw_path, raw_key="raw",
-        label_paths=label_path, label_key=None,
+        raw_paths=data_path, raw_key="raw",
+        label_paths=data_path, label_key="labels/nuclei",
         patch_shape=patch_shape, batch_size=batch_size,
         ndim=2, is_seg_dataset=True, rois=roi,
         label_transform=label_transform,
@@ -50,6 +49,7 @@ def run_training(name):
     )
 
 
+# NOTE: we don't need to do this, becuase we can now load the checkpoint directly
 def export_model(name):
     export_path = f"./{name}.pth"
     checkpoint_path = os.path.join("checkpoints", name, "best.pt")
@@ -62,8 +62,8 @@ def export_model(name):
 
 def main():
     name = "finetuned-vitb-em-nuclei"
-    # run_training(name)
-    export_model(name)
+    run_training(name)
+    # export_model(name)
 
 
 if __name__ == "__main__":
