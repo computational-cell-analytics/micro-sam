3e54953bef95a6f2f7d8253f260b35d5053494eb
Anwai Archit <52396323+anwai98@users.noreply.github.com>
2024-03-26 15:14:28 +0100

    Update run_all_finetuning.py

--- finetuning/run_all_finetuning.py
+++ finetuning/run_all_finetuning.py
@@ -39,7 +39,7 @@ source activate {env_name} \n"""
     python_script += f"-m {model_type} "
 
     # choice of the number of objects
-    python_script += f"--n_objects {N_OBJECTS[model_type[:5]]} "
+    python_script += f"--n_objects {N_OBJECTS[model_type]} "
 
     # let's add the python script to the bash script
     batch_script += python_script
@@ -76,7 +76,6 @@ def submit_slurm(args):
         "plantseg_root_specialist": "specialists/training/light_microscopy/plantseg_root_finetuning",
         "neurips_cellseg_specialist": "specialists/training/light_microscopy/neurips_cellseg_finetuning",
         "lm_generalist": "generalists/training/light_microscopy/train_lm_generalist",
-        "dynamicnuclearnet_specialist": "specialists/training/light_microscopy/dynamicnuclearnet_finetuning",
         "em_mito_nuc_generalist": "generalists/training/electron_microscopy/mito_nuc/train_mito_nuc_em_generalist",
         "em_boundaries_generalist": "generalists/training/electron_microscopy/boundaries/train_boundaries_em_generalist"
     }
