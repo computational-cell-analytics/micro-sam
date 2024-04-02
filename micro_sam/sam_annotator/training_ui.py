b21ff5e1b4009d4e497fd495af2175b8970265fd
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2024-04-02 22:24:09 +0200

    Update training UI to new GUI functionality WIP

--- micro_sam/sam_annotator/training_ui.py
+++ micro_sam/sam_annotator/training_ui.py
@@ -1,100 +1,152 @@
 import warnings
-from pathlib import Path
-from typing import Optional
+# from pathlib import Path
+# from typing import Optional
 
 import torch
 import torch_em
-from magicgui import magic_factory
+from napari.qt.threading import thread_worker
+from qtpy import QtWidgets
 from torch.utils.data import random_split
 
 import micro_sam.util as util
+import micro_sam.sam_annotator._widgets as widgets
 from micro_sam.training import default_sam_dataset, train_sam_for_setting, SETTINGS
 
 
 def _find_best_setting():
     if torch.cuda.is_available():
+        # TODO
         # can we check the GPU type and use it to match the setting?
         return "rtx5000"
     else:
         return "CPU"
 
 
-# TODO rethink some of the names
-# TODO add optional val paths
-@magic_factory(
-    call_button="Start Training",
-    setting={"choices": list(SETTINGS.keys())},
-    initial_model_name={"choices": list(util.models().urls.keys())},
-    patch_shape_x={"min": 0, "max": 2048},
-    patch_shape_y={"min": 0, "max": 2048},
-)
-def sam_training(
-    raw_path: Path,
-    label_path: Path,
-    raw_key: Optional[str] = None,
-    label_key: Optional[str] = None,
-    setting: str = _find_best_setting(),
-    train_instance_segmentation: bool = True,
-    name: Optional[str] = None,
-    # Can't set the max
-    # patch_shape: Tuple[int, int] = (512, 512),
-    patch_shape_x: int = 512,
-    patch_shape_y: int = 512,
-    initial_model_name: Optional[str] = None,
-    checkpoint_path: Optional[Path] = None,
-) -> None:
-    batch_size = 1
-    num_workers = 1 if setting == "CPU" else 4
+class TrainingWidget(widgets._WidgetBase):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
 
-    patch_shape = (patch_shape_x, patch_shape_y)
-    # TODO these should become optional params
-    raw_path_val, label_path_val = None, None
-    if raw_path_val is None:
-        dataset = default_sam_dataset(
-            str(raw_path), raw_key, str(label_path), label_key,
-            patch_shape=patch_shape, with_segmentation_decoder=train_instance_segmentation,
+        # Create the UI: the general options.
+        self.layout().addLayout(self._create_options())
+
+        # Add the settings (collapsible).
+        self.layout().addWidget(self._create_settings_widget())
+
+        # Add the run button to trigger the embedding computation.
+        self.run_button = QtWidgets.QPushButton("Start Training")
+        self.run_button.clicked.connect(self.__call__)
+        self.layout().addWidget(self.run_button)
+
+    def _create_options(self):
+        # TODO add raw_path, label_path, raw_key and label_key.
+        # we need _add_string_param and _add_path_param for this.
+        # wait for Luca's PR on this.
+        self.raw_path = None
+        self.raw_key = None
+
+        self.label_path = None
+        self.label_key = None
+
+        self.setting = _find_best_setting()
+        self._add_choice_param("setting", self.setting, list(SETTINGS.keys()))
+
+        self.train_instance_segmentation = True
+        self._add_boolean_param("train_instance_segmentation", self.train_instance_segmentation)
+
+    def _create_settings(self):
+        setting_values = QtWidgets.QWidget()
+        setting_values.setLayout(QtWidgets.QVBoxLayout())
+
+        # TODO use CPU instead of MPS on MAC because training with MPS is slower!
+        self.device = "auto"
+        device_options = ["auto"] + util._available_devices()
+        self.device_dropdown, layout = self._add_choice_param("device", self.device, device_options)
+        setting_values.layout().addLayout(layout)
+
+        self.patch_x, self.patch_y = 0, 0
+        self.patch_x_param, self.patch_y_param, layout = self._add_shape_param(
+            ("patch_x", "patch_y"), (self.patch_x, self.patch_y), min_val=0, max_val=2048
         )
-        # TODO better heuristic for the split?
-        train_dataset, val_dataset = random_split(dataset, lengths=[len(dataset) - 1, 1])
-    else:
-        if label_path_val is None:
-            raise ValueError
-        # TODO
-        train_dataset, val_dataset = "", ""
+        setting_values.layout().addLayout(layout)
 
-    train_loader = torch_em.segmentation.get_data_loader(
-        train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers,
-    )
-    val_loader = torch_em.segmentation.get_data_loader(
-        val_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers,
-    )
+        # TODO: the other optional params, also include stuff for val data!
+        self.name = None
+        self.initial_model_name = None
+        self.checkpoint_path = None
 
-    # Consolidate initial model name, the checkpoint path and the model type according to the settings.
-    if initial_model_name is None or initial_model_name == "None" or initial_model_name == "":
-        model_type = SETTINGS[setting]["model_type"]
-    else:
-        model_type = initial_model_name[:5]
-        if model_type != SETTINGS[setting]["model_type"]:
-            warnings.warn(
-                f"You have changed the model type for your chosen setting {setting} "
-                f"from {SETTINGS[setting]['model_type']} to {model_type}. "
-                "The training may be very slow or not work at all."
+        settings = widgets._make_collapsible(setting_values, title="Advanced")
+        return settings
+
+    def _get_loaders(self):
+        batch_size = 1
+        num_workers = 1 if str(self.device) == "cpu" else 4
+
+        patch_shape = (self.patch_shape_x, self.patch_shape_y)
+        # TODO use the values from optional params instead.
+        raw_path_val, label_path_val = None, None
+        if raw_path_val is None:
+            dataset = default_sam_dataset(
+                str(self.raw_path), self.raw_key, str(self.label_path), self.label_key,
+                patch_shape=patch_shape, with_segmentation_decoder=self.train_instance_segmentation,
             )
-    assert model_type is not None
+            # TODO better heuristic for the split?
+            train_dataset, val_dataset = random_split(dataset, lengths=[len(dataset) - 1, 1])
+        else:
+            if label_path_val is None:
+                raise ValueError
+            # TODO
+            train_dataset, val_dataset = "", ""
 
-    if checkpoint_path is None:
-        model_registry = util.models()
-        checkpoint_path = model_registry.fetch(model_type)
+        train_loader = torch_em.segmentation.get_data_loader(
+            train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers,
+        )
+        val_loader = torch_em.segmentation.get_data_loader(
+            val_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers,
+        )
+        return train_loader, val_loader
 
-    # We use the CPU if a GPU cannot be found.
-    # Note that using MPS on MAC is slower than training with the CPU!
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+    def _get_model_type(self):
+        # Consolidate initial model name, the checkpoint path and the model type according to the settings.
+        if self.self.initial_model_name is None or self.initial_model_name in ("None", ""):
+            model_type = SETTINGS[self.setting]["model_type"]
+        else:
+            model_type = self.initial_model_name[:5]
+            if model_type != SETTINGS[self.setting]["model_type"]:
+                warnings.warn(
+                    f"You have changed the model type for your chosen setting {self.setting} "
+                    f"from {SETTINGS[self.setting]['model_type']} to {model_type}. "
+                    "The training may be very slow or not work at all."
+                )
+        assert model_type is not None
+        return model_type
 
-    # TODO set napari pbar in torch_em
-    train_sam_for_setting(
-        name=name, setting=setting,
-        train_loader=train_loader, val_loader=val_loader,
-        checkpoint_path=checkpoint_path,
-        with_segmentation_decoder=train_instance_segmentation,
-        model_type=model_type, device=device
-    )
+    def __call__(self):
+        # Set up progress bar and signals for using it within a threadworker.
+        pbar, pbar_signals = widgets._create_pbar_for_threadworker()
+
+        model_type = self._get_model_type()
+        if self.checkpoint_path is None:
+            model_registry = util.models()
+            checkpoint_path = model_registry.fetch(model_type)
+        else:
+            checkpoint_path = self.checkpoint_path
+
+        @thread_worker()
+        def run_training():
+            train_loader, val_loader = self._get_loaders()
+            # TODO enable passing pbar callbacks to torch_em
+            train_sam_for_setting(
+                name=self.name, setting=self.setting,
+                train_loader=train_loader, val_loader=val_loader,
+                checkpoint_path=checkpoint_path,
+                with_segmentation_decoder=self.train_instance_segmentation,
+                model_type=model_type, device=self.device
+            )
+            # TODO export the model
+
+        worker = run_training()
+        # Note: this is how we can handle the worker when it's done.
+        # We can use this e.g. to add an indicator that the embeddings are computed or not.
+        # worker.returned.connect(lambda _: print("Embeddings for", self.model_type, "have been computed."))
+        worker.start()
+        return worker
