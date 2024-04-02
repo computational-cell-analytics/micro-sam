791c6bff7886320ca58bb3c0258789fb7d64d534
Anwai Archit <anwai.archit@gmail.com>
2024-04-02 23:45:13 +0200

    Add documentation for evaluation scripts

--- finetuning/evaluation/util.py
+++ finetuning/evaluation/util.py
@@ -220,7 +220,7 @@ def get_default_arguments():
     )
     parser.add_argument("-c", "--checkpoint", type=none_or_str, required=True, default=None)
     parser.add_argument("-e", "--experiment_folder", type=str, required=True)
-    parser.add_argument("-d", "--dataset", type=str, required=True)
+    parser.add_argument("-d", "--dataset", type=str, default=None)
     parser.add_argument("--box", action="store_true", help="If passed, starts with first prompt as box")
     parser.add_argument(
         "--use_masks", action="store_true", help="To use logits masks for iterative prompting."
