ed053c79dd3137525a9e2f57084b5c82cd3ea2e5
anwai98 <anwai.archit@gmail.com>
2024-04-04 11:02:33 +0200

    Refactor cellpose baseline

--- finetuning/evaluation/cellpose_baseline.py
+++ finetuning/evaluation/cellpose_baseline.py
@@ -1,13 +1,8 @@
 import os
 from tqdm import tqdm
-from glob import glob
 
-import z5py
-import numpy as np
 import pandas as pd
 import imageio.v3 as imageio
-
-from elf.evaluation import mean_segmentation_accuracy
 
 from micro_sam.evaluation.evaluation import run_evaluation
 
@@ -18,11 +13,18 @@ EXPERIMENT_ROOT = "/scratch/projects/nim00007/sam/experiments/benchmarking/cellp
 
 LM_DATASETS = [
     # in-domain (LM)
-    "tissuenet", "deepbacs", "plantseg/root", "livecell",
+    "tissuenet/one_chan", "tissuenet/multi_chan", "deepbacs", "plantseg/root", "livecell",
     "neurips-cell-seg/all", "neurips-cell-seg/tuning", "neurips-cell-seg/self",
     # out-of-domain (LM)
     "covid_if", "plantseg/ovules", "hpa", "lizard", "mouse-embryo", "ctc/hela_samples", "dynamicnuclearnet", "pannuke"
 ]
+
+FOR_MULTICHAN = ["tissuenet/multi_chan"]
+
+# For TissueNet:
+# RESULTS:
+# mSA score for inference at channel [1, 3]: 0.28846337471846234
+# mSA score for inference at channel [2, 3]: 0.4309667789626076
 
 
 def load_cellpose_model(model_type):
@@ -52,12 +54,18 @@ def run_cellpose_segmentation(dataset, model_type):
         if os.path.exists(out_path):
             continue
         image = imageio.imread(path)
+        channels = [0, 0]  # it's assumed to use one-channel, unless overwritten by logic below
+
         if image.ndim == 3:
             assert image.shape[-1] == 3
-            image = image.mean(axis=-1)
-        assert image.ndim == 2
-        seg = model.eval(image, diameter=None, flow_threshold=None, channels=[0, 0])[0]
-        assert seg.shape == image.shape
+            if dataset in FOR_MULTICHAN:
+                channels = [2, 3]
+            else:
+                image = image.mean(axis=-1)
+
+        seg = model.eval(image, diameter=None, flow_threshold=None, channels=channels)[0]
+
+        assert seg.shape == image.shape[:2]
         imageio.imwrite(out_path, seg, compression=5)
 
     return prediction_folder
@@ -91,41 +99,6 @@ def run_cellpose_baseline(datasets, model_types):
             evaluate_dataset(prediction_folder, dataset, model_type)
 
 
-def test_cellpose(model_type, view=False):
-    # data_dir = "/scratch/projects/nim00007/sam/data/tissuenet/test"
-    data_dir = "/media/anwai/ANWAI/data/tissuenet/test"
-    all_tissunet_paths = sorted(glob(os.path.join(data_dir, "*.zarr")))
-
-    model = load_cellpose_model(model_type)
-
-    msa13_list, msa23_list = [], []
-    for chosen_vol_path in tqdm(all_tissunet_paths):
-        with z5py.File(chosen_vol_path, "a", use_zarr_format=True) as f:
-            raw = f["raw/rgb"][:]
-            labels = f["labels/cell"][:]
-            prediction13 = model.eval(raw, diameter=None, flow_threshold=None, channels=[1, 3])[0]
-            prediction23 = model.eval(raw, diameter=None, flow_threshold=None, channels=[2, 3])[0]  # NOTE: BETTER
-
-            msa13_list.append(mean_segmentation_accuracy(prediction13, labels))
-            msa23_list.append(mean_segmentation_accuracy(prediction23, labels))
-
-            if view:
-                import napari
-                v = napari.Viewer()
-                v.add_image(raw)
-                v.add_labels(labels)
-                v.add_labels(prediction13)
-                v.add_labels(prediction23)
-                napari.run()
-
-    print("mSA score for inference at channel [1, 3]:", np.mean(msa13_list))
-    print("mSA score for inference at channel [2, 3]:", np.mean(msa23_list))
-
-    # RESULTS:
-    # mSA score for inference at channel [1, 3]: 0.28846337471846234
-    # mSA score for inference at channel [2, 3]: 0.4309667789626076
-
-
 def main(args):
     if args.dataset is None:
         datasets = LM_DATASETS
@@ -134,14 +107,11 @@ def main(args):
         assert datasets in LM_DATASETS
 
     if args.model_type is None:
-        model_types = ["cyto", "cyto2", "cyto3", "nuclei", "livecell", "tissuenet"]
+        model_types = ["cyto", "cyto2", "cyto3", "livecell", "tissuenet"]
     else:
         model_types = args.model_type
 
-    if args.custom:
-        test_cellpose(model_types, view=False)
-    else:
-        run_cellpose_baseline(datasets, model_types)
+    run_cellpose_baseline(datasets, model_types)
 
 
 if __name__ == "__main__":
@@ -149,6 +119,5 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--dataset", type=str, default=None)
     parser.add_argument("-m", "--model_type", type=str, default=None)
-    parser.add_argument("--custom", action="store_true")
     args = parser.parse_args()
     main(args)
