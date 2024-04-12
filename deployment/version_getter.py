e4a45cc312af313e2b596a4cf7f1e6c0947b7de5
lufre1 <155526548+lufre1@users.noreply.github.com>
2024-04-12 20:42:15 +0200

    505 update installer scripts (#510)
    Update workflow for building the installers

--- deployment/version_getter.py
+++ deployment/version_getter.py
@@ -6,10 +6,10 @@ yaml = ruamel.yaml.YAML()
 yaml.preserve_quotes = True
 ctor_fname = os.path.join("construct.yaml")
 
-with open(ctor_fname, 'r') as stream:
+with open(ctor_fname, "r") as stream:
     ctor_conf = yaml.load(stream)
 
 ctor_conf["version"] = runpy.run_path(os.path.join("..", "micro_sam", "__version__.py"))["__version__"]
 
-with open(ctor_fname, 'w') as outfile:
+with open(ctor_fname, "w") as outfile:
     yaml.dump(ctor_conf, outfile)
