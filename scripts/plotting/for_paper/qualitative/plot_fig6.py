5551a107e4dba218bbc6aed8f9e82c858e1519d9
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2024-04-16 12:56:02 +0200

    Add script for fig 6 WIP

--- scripts/plotting/for_paper/qualitative/plot_fig6.py
+++ scripts/plotting/for_paper/qualitative/plot_fig6.py
@@ -6,18 +6,21 @@ import napari
 DATA_ROOT = "./figure_data"
 
 
-# FIXME this is still the wrong raw data.
 def plot_3d():
     path = os.path.join(DATA_ROOT, "fig_6", "3d", "data_user_study_3d.h5")
     with h5py.File(path, "r") as f:
         raw = f["raw"][:]
-        seg_sam = f["segmentation/finetuned"][:]
-        seg_ilastik = f["segmentation/ilastik"][:].squeeze()
+        labels = f["labels"][:]
+        seg_ilastik = f["segmentation/ilastik"][:]
+        seg_default = f["segmentation/sam_default"][:]
+        seg_finetuned = f["segmentation/sam_finetuned"][:]
 
     v = napari.Viewer()
     v.add_image(raw)
-    v.add_labels(seg_sam)
     v.add_labels(seg_ilastik)
+    v.add_labels(seg_default)
+    v.add_labels(seg_finetuned)
+    v.add_labels(labels)
     napari.run()
 
 
