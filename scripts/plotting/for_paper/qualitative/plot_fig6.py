b2384b627788bf4f9ad320bbab9bda65964f4691
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2024-04-16 14:26:36 +0200

    Add data creation and visualization for 2d user study

--- scripts/plotting/for_paper/qualitative/plot_fig6.py
+++ scripts/plotting/for_paper/qualitative/plot_fig6.py
@@ -1,5 +1,7 @@
 import os
+from glob import glob
 
+import imageio.v3 as imageio
 import h5py
 import napari
 
@@ -24,4 +26,81 @@ def plot_3d():
     napari.run()
 
 
-plot_3d()
+def create_data_2d_default():
+    from micro_sam.util import get_sam_model
+    from micro_sam.instance_segmentation import get_amg, mask_data_to_segmentation
+
+    model = get_sam_model(model_type="vit_b")
+    segmenter = get_amg(model, is_tiled=False, decoder=None)
+
+    images = glob("./organoids/images/*.tif")
+    assert len(images) > 0
+    out_folder = "./organoids/seg_default"
+    os.makedirs(out_folder, exist_ok=True)
+
+    for im in images:
+        print("Segmenting", im, "...")
+        image = imageio.imread(im)
+        segmenter.initialize(image, verbose=True)
+        seg = segmenter.generate()
+        seg = mask_data_to_segmentation(seg, with_background=True)
+        imageio.imwrite(os.path.join(out_folder, os.path.basename(im)), seg, compression="zlib")
+
+
+def create_data_2d_finetuned():
+    from micro_sam.instance_segmentation import get_predictor_and_decoder, get_amg, mask_data_to_segmentation
+
+    model, decoder = get_predictor_and_decoder("vit_b", "./organoids/vit_b_organoids.pt")
+    segmenter = get_amg(model, is_tiled=False, decoder=decoder)
+
+    images = glob("./organoids/images/*.tif")
+    assert len(images) > 0
+    out_folder = "./organoids/seg_finetuned"
+    os.makedirs(out_folder, exist_ok=True)
+
+    for im in images:
+        print("Segmenting", im, "...")
+        image = imageio.imread(im)
+        segmenter.initialize(image, verbose=True)
+        seg = segmenter.generate(image)
+        seg = mask_data_to_segmentation(seg, with_background=True)
+        imageio.imwrite(os.path.join(out_folder, os.path.basename(im)), seg, compression="zlib")
+
+
+def plot_2d():
+    image_root = os.path.join(DATA_ROOT, "fig_6", "2d", "images")
+    seg_finetuned_root = os.path.join(DATA_ROOT, "fig_6", "2d", "seg_finetuned")
+    seg_default_root = os.path.join(DATA_ROOT, "fig_6", "2d", "seg_default")
+
+    images = glob(os.path.join(image_root, "*.tif"))
+
+    for im in images:
+        image = imageio.imread(im)
+
+        default_path = os.path.join(seg_default_root, os.path.basename(im))
+        seg_default = imageio.imread(default_path)
+
+        finetuned_path = os.path.join(seg_finetuned_root, os.path.basename(im))
+        seg_finetuned = imageio.imread(finetuned_path)
+
+        v = napari.Viewer()
+        v.add_image(image)
+        v.add_labels(seg_finetuned)
+        v.add_labels(seg_default)
+        napari.run()
+
+
+# TODO
+def plot_tracking():
+    pass
+
+
+def main():
+    # create_data_2d_default()
+    # create_data_2d_finetuned()
+
+    # plot_3d()
+    plot_2d()
+
+
+main()
