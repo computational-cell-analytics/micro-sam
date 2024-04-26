cb384e44af64cb1a0d89cd33c1ffad5f871f38eb
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2024-04-26 21:26:33 +0200

    Update state precomputation and extend test

--- test/test_sam_annotator/test_cli.py
+++ test/test_sam_annotator/test_cli.py
@@ -1,8 +1,24 @@
+import os
 import unittest
-from shutil import which
+from shutil import which, rmtree
+from subprocess import run
+
+import imageio.v3 as imageio
+import micro_sam.util as util
+import zarr
+from skimage.data import binary_blobs
 
 
 class TestCLI(unittest.TestCase):
+    model_type = "vit_t" if util.VIT_T_SUPPORT else "vit_b"
+    tmp_folder = "tmp-files"
+
+    def setUp(self):
+        os.makedirs(self.tmp_folder, exist_ok=True)
+
+    def tearDown(self):
+        rmtree(self.tmp_folder)
+
     def _test_command(self, cmd):
         self.assertTrue(which(cmd) is not None)
 
@@ -20,6 +36,42 @@ class TestCLI(unittest.TestCase):
 
     def test_precompute_embeddings(self):
         self._test_command("micro_sam.precompute_embeddings")
+
+        # Create 3 images as testdata.
+        for i in range(3):
+            im_path = os.path.join(self.tmp_folder, f"image-{i}.tif")
+            image_data = binary_blobs(512).astype("uint8") * 255
+            imageio.imwrite(im_path, image_data)
+
+        # Test precomputation with a single image.
+        emb_path1 = os.path.join(self.tmp_folder, "embedddings1.zarr")
+        run([
+            "micro_sam.precompute_embeddings", "-i", im_path, "-e", emb_path1,
+            "-m", self.model_type
+        ])
+        self.assertTrue(os.path.exists(emb_path1))
+        with zarr.open(emb_path1, "r") as f:
+            self.assertIn("features", f)
+
+        # Test precomputation with image stack.
+        emb_path2 = os.path.join(self.tmp_folder, "embedddings2.zarr")
+        run([
+            "micro_sam.precompute_embeddings", "-i", self.tmp_folder, "-e", emb_path2,
+            "-m", self.model_type, "-k", "*.tif"
+        ])
+        self.assertTrue(os.path.exists(emb_path2))
+        with zarr.open(emb_path2, "r") as f:
+            self.assertIn("features", f)
+            self.assertEqual(f["features"].shape[0], 3)
+
+        # Test precomputation with pattern to process multiple image.
+        emb_path3 = os.path.join(self.tmp_folder, "embedddings3")
+        run([
+            "micro_sam.precompute_embeddings", "-i", self.tmp_folder, "-e", emb_path3,
+            "-m", self.model_type, "--pattern", "*.tif"
+        ])
+        for i in range(3):
+            self.assertTrue(os.path.exists(os.path.join(emb_path3, f"image-{i}.zarr")))
 
 
 if __name__ == "__main__":
