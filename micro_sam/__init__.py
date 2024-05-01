1ccd662a08c89dc3b6fd7d780e5a38b3070c0ab3
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2024-05-01 22:25:31 +0200

    Add FAQ into doc and revise it (WIP)

--- micro_sam/__init__.py
+++ micro_sam/__init__.py
@@ -4,6 +4,7 @@
 .. include:: ../doc/annotation_tools.md
 .. include:: ../doc/python_library.md
 .. include:: ../doc/finetuned_models.md
+.. include:: ../doc/faq.md
 .. include:: ../doc/contributing.md
 .. include:: ../doc/development.md
 .. include:: ../doc/band.md
