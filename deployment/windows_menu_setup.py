55088639577a555e14d55ed97ddd072e5557f414
Luca Freckmann <luca.freckmann@gmail.com>
2024-05-02 10:31:01 +0200

    added logo.ico and changed paths for the windows installer accordingly

--- deployment/windows_menu_setup.py
+++ deployment/windows_menu_setup.py
@@ -5,6 +5,6 @@ import os
 conda_dir = os.environ["CONDA_PREFIX"]
 menu_dir = os.path.join(conda_dir, "Menu")
 os.makedirs(menu_dir, exist_ok=True)
-shutil.copy("../doc/logo/logo.png", menu_dir)
+shutil.copy("../doc/logo/logo.ico", menu_dir)
 shutil.copy("./windows_menu.json", menu_dir)
 menuinst.install(os.path.join(menu_dir, 'windows_menu.json'), prefix=conda_dir)
