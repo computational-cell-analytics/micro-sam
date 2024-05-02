0090fe93c9d100843b6cf794d4003e69f763e822
lufre1 <155526548+lufre1@users.noreply.github.com>
2024-05-02 14:39:18 +0200

    added windows installer logo.ico and changed paths accordingly (#575)
    Added windows installer logo.ico and changed paths accordingly

--- deployment/windows_menu_setup.py
+++ deployment/windows_menu_setup.py
@@ -4,7 +4,7 @@ import os
 
 conda_dir = os.environ["CONDA_PREFIX"]
 menu_dir = os.path.join(conda_dir, "Menu")
-os.makedirs(menu_dir, exist_ok = True)
-shutil.copy("../doc/images/micro-sam-logo.ico", menu_dir)
+os.makedirs(menu_dir, exist_ok=True)
+shutil.copy("../doc/logo.ico", menu_dir)
 shutil.copy("./windows_menu.json", menu_dir)
 menuinst.install(os.path.join(menu_dir, 'windows_menu.json'), prefix=conda_dir)
