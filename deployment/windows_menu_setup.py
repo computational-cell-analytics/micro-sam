debf7868a7fc88e05841eb7f95a58a4393efc543
Luca Freckmann <luca.freckmann@gmail.com>
2024-05-02 14:15:15 +0200

    corrected path in windows_menu_setup.py

--- deployment/windows_menu_setup.py
+++ deployment/windows_menu_setup.py
@@ -5,6 +5,6 @@ import os
 conda_dir = os.environ["CONDA_PREFIX"]
 menu_dir = os.path.join(conda_dir, "Menu")
 os.makedirs(menu_dir, exist_ok=True)
-shutil.copy("../doc/images/logo.ico", menu_dir)
+shutil.copy("../doc/logo.ico", menu_dir)
 shutil.copy("./windows_menu.json", menu_dir)
 menuinst.install(os.path.join(menu_dir, 'windows_menu.json'), prefix=conda_dir)
