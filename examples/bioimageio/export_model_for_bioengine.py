74af1b625374532b8898ec4d465d012dff075408
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2024-05-10 10:20:54 +0200

    Fix CI

--- examples/bioimageio/export_model_for_bioengine.py
+++ examples/bioimageio/export_model_for_bioengine.py
@@ -1,3 +1,3 @@
-from micro_sam.modelzoo.bioengine_export import export_bioengine_model
+from micro_sam.bioimageio.bioengine_export import export_bioengine_model
 
-export_bioengine_model("vit_b", "test-export", opset=12)
+export_bioengine_model("vit_t", "test-export", opset=12)
