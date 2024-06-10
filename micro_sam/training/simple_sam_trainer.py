7db9a030da81e4d008d0ed01c4f56da8f513bfae
anwai98 <anwai.archit@gmail.com>
2024-06-10 18:07:11 +0200

    Update simple sam trainer to randomly chose prompts while not instructed

--- micro_sam/training/simple_sam_trainer.py
+++ micro_sam/training/simple_sam_trainer.py
@@ -1,3 +1,5 @@
+import random
+
 from . import SamTrainer
 
 
@@ -14,20 +16,37 @@ class SimpleSamTrainer(SamTrainer):
         self.use_points = use_points
         self.use_box = use_box
 
-        if (self.use_points + self.use_box) != 1:
-            raise ValueError("You must choose either 'use_points' / 'use_box'.")
+        if not self.use_points and not self.use_box:  # if user doesn't specify, we randomly choose box / point prompts
+            self.random_prompt_choice = True
+        else:
+            self.random_prompt_choice = False
+
+        assert (self.use_points + self.use_box) < 2, "Please choose either of the prompt-based segmentation."
+
+    def _choose_one_positive_point(self):
+        "samples only a single positive point per object"
+        n_pos, n_neg = 1, 0
+        multimask_output = True
+        return n_pos, n_neg, None, multimask_output
+
+    def _choose_box(self):
+        "samples only a single box per object"
+        n_pos, n_neg = 0, 0
+        multimask_output = False
+        get_boxes = True
+        return n_pos, n_neg, get_boxes, multimask_output
 
     def _get_prompt_and_multimasking_choices(self, current_iteration):
-        if self.use_points:
-            n_pos, n_neg = 1, 0  # samples only a single positive point per object
-            multimask_output = True
+
+        if self.random_prompt_choice:
+            available_choices = [self._choose_one_positive_point(), self._choose_box()]
+            return random.choice(available_choices)
         else:
-            n_pos, n_neg = 0, 0
-            multimask_output = False
-
-        get_boxes = self.use_box  # samples only a single box per object
-
-        return n_pos, n_neg, get_boxes, multimask_output
+            # this condition supports using either "point" / "box" / "points + box"
+            if self.use_points:
+                return self._choose_one_positive_point()
+            else:
+                return self._choose_box()
 
     def _get_prompt_and_multimasking_choices_for_val(self, current_iteration):
         return self._get_prompt_and_multimasking_choices(current_iteration)
