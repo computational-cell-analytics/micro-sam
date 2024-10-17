2caf5f37f936e5a8374ce81cf46f11ee31a90922
Anwai Archit <anwai.archit@gmail.com>
2024-10-17 18:49:56 +0200

    Add documentation

---
+++ workshops/README.md
@@ -0,0 +1,35 @@
+# Hands-On Analysis using `micro-sam`
+
+## Upcoming Workshops:
+1. I2K 2024 (Milan, Italy)
+2. Virtual I2K 2024 (Online)
+
+## Introduction
+
+In this document, we walk you through different steps involved to participate in hands-on image annotation experiments our tool.
+
+- Here is our [official documentation](https://computational-cell-analytics.github.io/micro-sam/) for detailed explanation of our tools, library and the finetuned models.
+- Here is the playlist for our [tutorial videos](https://youtube.com/playlist?list=PLwYZXQJ3f36GQPpKCrSbHjGiH39X4XjSO&si=3q-cIRD6KuoZFmAM) hosted on YouTube, elaborating in detail on the features of our tools.
+
+## Steps:
+
+### Step 1: Download the Datasets
+
+- We provide the script `download_datasets.py` for automatic download of datasets to be used for interactive annotation using `micro-sam`.
+- You can run the script as follows:
+```bash
+$ python download_datasets.py -i <DATA_DIRECTORY> -d <DATASET_NAME>
+```
+where, `DATA_DIRECTORY` is the filepath where the datasets will be downloaded, and `DATASET_NAME` is the name of the dataset (see `python download_datasets.py -h` for the details)
+
+### Step 2: Download the Precomputed Embeddings
+
+- ...
+
+### Step 3: Run the `micro-sam` Annotators
+
+- ...
+
+### Step 4: Finetune Segment Anything on Microscopy Images
+
+- ...
--- workshops/download_datasets.py
+++ workshops/download_datasets.py
@@ -1,23 +1,25 @@
 import os
 
 from torch_em.data import datasets
+from torch_em.util.image import load_data
 
 
 def _get_cellpose_sample_data(path, download):
-    raw_paths = ...
-    label_paths = ...
-
+    # TODO: update to self-chosen samples.
+    raw_paths, label_paths = datasets.cellpose.get_cellpose_paths(
+        path=path, split="test", choice="cyto", download=download
+    )
     return raw_paths, label_paths
 
 
-def _get_dataset_paths(path, dataset_name):
-    all_datasets = {
+def _get_dataset_paths(path, dataset_name, view=False):
+    dataset_paths = {
         # 2d LM dataset for cell segmentation
         "cellpose": lambda: _get_cellpose_sample_data(path=os.path.join(path, "cellpose"), download=True),
 
-        # 3d LM dataset fro nuclei segmentation
+        # 3d LM dataset for nuclei segmentation
         "embedseg": lambda: datasets.embedseg_data.get_embedseg_paths(
-            path=os.path.join(path, "embedseg"), name="Mouse-Skull-Nuclei-CBG", split="test", download=True,
+            path=os.path.join(path, "embedseg"), name="Mouse-Skull-Nuclei-CBG", split="train", download=True,
         ),
 
         # 3d EM dataset for membrane segmentation
@@ -26,13 +28,50 @@ def _get_dataset_paths(path, dataset_name):
         ),
     }
 
+    dataset_keys = {
+        "cellpose": [None, None],
+        "embedseg": [None, None],
+        "platynereis_cells": ["volumes/raw/s1", "volumes/labels/segmentation/s1"]
+    }
+
     if dataset_name is None:
         dataset_names = [dataset_name]
     else:
         dataset_names = [dataset_name]
 
     for dname in dataset_names:
-        all_datasets[dname]()
+        if dname not in dataset_paths:
+            raise ValueError(
+                f"'{dname}' is not a supported dataset enabled for download. "
+                f"Please choose from {list(dataset_paths.keys())}."
+            )
+
+        paths = dataset_paths[dname]()
+        print(f"'{dataset_name}' is download at {path}.")
+
+        # platynereis/membrane/train_data_membrane_02.n5 (Platynereis Cells)
+        # embedseg/Mouse-Skull-Nuclei-CBG/train/images/X1.tif (EmbedSeg)
+
+        if view:
+            import napari
+
+            if isinstance(paths, tuple):  # datasets with explicit raw and label paths
+                raw_paths, label_paths = paths
+            else:
+                raw_paths = label_paths = paths
+
+            raw_key, label_key = dataset_keys[dname]
+            for raw_path, label_path in zip(raw_paths, label_paths):
+                print(raw_path)
+                raw = load_data(raw_path, raw_key)
+                labels = load_data(label_path, label_key)
+
+                v = napari.Viewer()
+                v.add_image(raw)
+                v.add_labels(labels)
+                napari.run()
+
+                # break  # comment this line out in case you would like to visualize all samples.
 
 
 def main():
@@ -40,9 +79,10 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input_path", type=str, default="./data")
     parser.add_argument("-d", "--dataset_name", type=str, default=None)
+    parser.add_argument("--view", action="store_true")
     args = parser.parse_args()
 
-    _get_dataset_paths(path=args.input_path, dataset_name=args.dataset_name)
+    _get_dataset_paths(path=args.input_path, dataset_name=args.dataset_name, view=args.view)
 
 
 if __name__ == "__main__":
