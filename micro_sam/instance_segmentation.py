6b9f058b0d787ef030cb16875485f64a8ed392d3
Carolin <carolin.teuber@stud.uni-goettingen.de>
2024-11-13 15:22:22 +0100

    set dim=2 for embedding computation

--- micro_sam/instance_segmentation.py
+++ micro_sam/instance_segmentation.py
@@ -907,7 +907,7 @@ class InstanceSegmentationWithDecoder:
         pbar_init(1, "Initialize instance segmentation with decoder")
 
         if image_embeddings is None:
-            image_embeddings = util.precompute_image_embeddings(self._predictor, image)
+            image_embeddings = util.precompute_image_embeddings(self._predictor, image, ndim=2)
 
         # Get the image embeddings from the predictor.
         self._predictor = util.set_precomputed(self._predictor, image_embeddings, i=i)
