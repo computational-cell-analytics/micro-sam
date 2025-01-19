c8c38ff8a7890d8752f7f0d4e5dd041e85c583a3
anwai98 <anwai.archit@gmail.com>
2025-01-19 18:15:04 +0100

    Refactor value checks

--- micro_sam/training/semantic_sam_trainer.py
+++ micro_sam/training/semantic_sam_trainer.py
@@ -70,6 +70,9 @@ class SemanticSamTrainer(DefaultTrainer):
         self.compute_ce_loss = nn.CrossEntropyLoss()
         self.dice_weight = dice_weight
 
+        if self.dice_weight is not None and (self.dice_weight < 0 or self.dice_weight > 1):
+            raise ValueError("The weight factor should lie between 0 and 1.")
+
         self._kwargs = kwargs
 
     def _compute_loss(self, y, masks):
