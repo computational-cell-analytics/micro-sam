db98600d259f3ed092b4991b2cdbd3569cc22ec3
anwai98 <anwai.archit@gmail.com>
2025-01-19 18:12:10 +0100

    Minor update to remove dice weight constraints for semantic trainer

--- micro_sam/training/semantic_sam_trainer.py
+++ micro_sam/training/semantic_sam_trainer.py
@@ -70,9 +70,6 @@ class SemanticSamTrainer(DefaultTrainer):
         self.compute_ce_loss = nn.CrossEntropyLoss()
         self.dice_weight = dice_weight
 
-        if self.dice_weight is not None:
-            assert self.dice_weight > 0 and self.dice_weight < 1, "The weight factor should lie between 0 and 1."
-
         self._kwargs = kwargs
 
     def _compute_loss(self, y, masks):
