69545cd94df6f244383dd0a8c287ccb980182be5
anwai98 <anwai.archit@gmail.com>
2025-02-04 15:21:38 +0100

    Make rank checks strict for lora and fact

--- micro_sam/models/peft_sam.py
+++ micro_sam/models/peft_sam.py
@@ -315,7 +315,7 @@ class PEFT_Sam(nn.Module):
     ):
         super().__init__()
 
-        if issubclass(peft_module, Union[LoRASurgery, FacTSurgery]) and rank <= 0:
+        if issubclass(peft_module, Union[LoRASurgery, FacTSurgery]) and (not rank or rank <= 0):
             raise RuntimeError("The chosen PEFT method cannot run without a valid rank choice.")
 
         assert issubclass(peft_module, Union[LoRASurgery, FacTSurgery, SelectiveSurgery, SSFSurgery, AdaptFormer]), (
