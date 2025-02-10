8b4089c12a131556793ac4d9e537cb7432ad9ec6
anwai98 <anwai.archit@gmail.com>
2025-02-10 19:50:19 +0100

    Finalize result verification

--- scripts/plotting/for_release/run_inference_for_new_benchmarking.py
+++ scripts/plotting/for_release/run_inference_for_new_benchmarking.py
@@ -9,32 +9,32 @@ ROOT = "/home/nimanwai/micro-sam/development"
 
 
 def get_comparison_plot_for_new_models(metric, model_type):
-    old_mfolder = "vit_b_old_model" if model_type == "vit_b_lm" else "vit_l_old_model"
-    new_mfolder = "vit_b_new_model" if model_type == "vit_b_lm" else "vit_l_new_model"
+    old_mfolder = f"{model_type}_old_model"
+    new_mfolder = f"{model_type}_new_model"
 
     old_results = natsorted(glob(os.path.join(ROOT, old_mfolder, "*", "results", "ais_2d.csv")))
     new_results = natsorted(glob(os.path.join(ROOT, new_mfolder, "*", "results", "ais_2d.csv")))
 
     results = []
     for old_res_path, new_res_path in zip(old_results, new_results):
-        old_res = pd.read_csv(old_res_path)
-        new_res = pd.read_csv(new_res_path)
-
         dname = old_res_path.rsplit("/")[-3]
+        assert dname in new_res_path, (old_res_path, new_res_path)
+
         res = {
             "dataset": dname,
-            model_type: old_res[metric][0],
-            f"{model_type} (NEW)": new_res[metric][0],
+            f"{model_type}_lm": pd.read_csv(old_res_path)[metric][0],
+            f"{model_type}_lm (NEW)": pd.read_csv(new_res_path)[metric][0],
         }
         results.append(pd.DataFrame.from_dict([res]))
 
     results = pd.concat(results, ignore_index=True)
     print(results)
-    breakpoint()
+    print()
 
 
 def main():
-    get_comparison_plot_for_new_models(metric="mSA", model_type="vit_b_lm")
+    get_comparison_plot_for_new_models(metric="mSA", model_type="vit_b")
+    get_comparison_plot_for_new_models(metric="mSA", model_type="vit_l")
 
 
 main()
