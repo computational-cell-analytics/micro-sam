1c6c10f03956be5dfd4176884bf36820b36a88c1
Peter Sobolewski <76622105+psobolewskiPhD@users.noreply.github.com>
2025-03-06 12:59:31 -0500

    ensure the return of _to_image is a np.array

--- micro_sam/util.py
+++ micro_sam/util.py
@@ -567,7 +567,9 @@ def _to_image(input_):
     else:
         raise ValueError(f"Invalid input image of shape {input_.shape}. Expect either 2D grayscale or 3D RGB image.")
 
-    return image
+    # explicitly return a numpy array for compatibility with torchvision
+    # because the input_ array could be something like dask array
+    return np.array(image)
 
 
 def _compute_tiled_features_2d(predictor, input_, tile_shape, halo, f, pbar_init, pbar_update):
