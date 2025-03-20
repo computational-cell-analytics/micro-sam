742b4143f8d996fdd8141b17d08387ad23019ca5
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2025-03-20 20:11:21 +0100

    Simplify scale handling in _update_image

--- micro_sam/sam_annotator/_annotator.py
+++ micro_sam/sam_annotator/_annotator.py
@@ -179,17 +179,20 @@ class _AnnotatorBase(QtWidgets.QScrollArea):
                 )
             self._shape = state.image_shape
 
+        # Update the image scale.
+        scale = state.image_scale
+
         # Reset all layers.
         self._viewer.layers["current_object"].data = np.zeros(self._shape, dtype="uint32")
-        self._viewer.layers["current_object"].scale = state.image_scale
+        self._viewer.layers["current_object"].scale = scale
         self._viewer.layers["auto_segmentation"].data = np.zeros(self._shape, dtype="uint32")
-        self._viewer.layers["auto_segmentation"].scale = state.image_scale
+        self._viewer.layers["auto_segmentation"].scale = scale
         if segmentation_result is None or segmentation_result is False:
             self._viewer.layers["committed_objects"].data = np.zeros(self._shape, dtype="uint32")
         else:
             assert segmentation_result.shape == self._shape
             self._viewer.layers["committed_objects"].data = segmentation_result
-        self._viewer.layers["committed_objects"].scale = state.image_scale
-        self._viewer.layers["point_prompts"].scale = state.image_scale
-        self._viewer.layers["prompts"].scale = state.image_scale
+        self._viewer.layers["committed_objects"].scale = scale
+        self._viewer.layers["point_prompts"].scale = scale
+        self._viewer.layers["prompts"].scale = scale
         vutil.clear_annotations(self._viewer, clear_segmentations=False)
