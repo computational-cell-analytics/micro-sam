02e93bb032bb57557494765a627763f829f8b8bb
anwai98 <anwai.archit@gmail.com>
2025-11-27 10:48:15 +0100

    Extend baseline scripts

--- scripts/apg_experiments/prepare_baselines.py
+++ scripts/apg_experiments/prepare_baselines.py
@@ -1,17 +1,10 @@
-import os
-from glob import glob
 from tqdm import tqdm
 
 import numpy as np
 import imageio.v3 as imageio
 
-from torch_em.data.datasets.light_microscopy.livecell import get_livecell_paths
-
 from elf.evaluation import mean_segmentation_accuracy, matching
 
-from micro_sam.evaluation.inference import run_apg
-from micro_sam.evaluation.evaluation import run_evaluation
-from micro_sam.evaluation.livecell import _get_livecell_paths
 from micro_sam.automatic_segmentation import get_predictor_and_segmenter, automatic_instance_segmentation
 from micro_sam.instance_segmentation import (
     AutomaticPromptGenerator, get_predictor_and_decoder, mask_data_to_segmentation
@@ -20,11 +13,14 @@ from micro_sam.instance_segmentation import (
 from tukra.inference.get_cellpose import segment_using_cellpose
 from tukra.inference.get_instanseg import segment_using_instanseg
 
+from util import get_image_label_paths
+
+
+NAME = "dsb"
+
 
 def run_default_ais(model_type="vit_b_lm"):
-    # Get the image paths for LIVECell.
-    data_dir = "/mnt/vast-nhr/projects/cidas/cca/data/livecell"
-    image_paths, label_paths = get_livecell_paths(path=data_dir, split="test")
+    image_paths, label_paths = get_image_label_paths(dataset_name=NAME, split="test")
 
     # Prepare the predictor
     predictor, segmenter = get_predictor_and_segmenter(model_type=model_type)
@@ -58,10 +54,7 @@ def run_default_ais(model_type="vit_b_lm"):
 
 
 def run_default_apg(model_type="vit_b_lm"):
-    # Get the image paths for LIVECell.
-    # data_dir = "/mnt/vast-nhr/projects/cidas/cca/data/livecell"
-    data_dir = "/home/anwai/data/livecell"
-    image_paths, label_paths = get_livecell_paths(path=data_dir, split="test")
+    image_paths, label_paths = get_image_label_paths(dataset_name=NAME, split="test")
 
     # Prepare the predictor
     predictor, decoder = get_predictor_and_decoder(model_type=model_type)
@@ -98,9 +91,7 @@ def run_default_apg(model_type="vit_b_lm"):
 
 
 def run_default_amg(model_type="vit_b_lm"):
-    # Get the image paths for LIVECell.
-    data_dir = "/mnt/vast-nhr/projects/cidas/cca/data/livecell"
-    image_paths, label_paths = get_livecell_paths(path=data_dir, split="test")
+    image_paths, label_paths = get_image_label_paths(dataset_name=NAME, split="test")
 
     # Prepare the predictor
     predictor, segmenter = get_predictor_and_segmenter(model_type=model_type, amg=True)
@@ -134,9 +125,7 @@ def run_default_amg(model_type="vit_b_lm"):
 
 
 def run_default_cellpose(model_choice):
-    # Get the image paths for LIVECell.
-    data_dir = "/mnt/vast-nhr/projects/cidas/cca/data/livecell"
-    image_paths, label_paths = get_livecell_paths(path=data_dir, split="test")
+    image_paths, label_paths = get_image_label_paths(dataset_name=NAME, split="test")
 
     msas, sa50s, precisions, recalls, f1s = [], [], [], [], []
     for curr_image_path, curr_label_path in tqdm(
@@ -167,9 +156,7 @@ def run_default_cellpose(model_choice):
 def run_default_cellsam():
     from cellSAM import cellsam_pipeline
 
-    # Get the image paths for LIVECell.
-    data_dir = "/mnt/vast-nhr/projects/cidas/cca/data/livecell"
-    image_paths, label_paths = get_livecell_paths(path=data_dir, split="test")
+    image_paths, label_paths = get_image_label_paths(dataset_name=NAME, split="test")
 
     msas, sa50s, precisions, recalls, f1s = [], [], [], [], []
     for curr_image_path, curr_label_path in tqdm(
@@ -204,9 +191,7 @@ def run_default_cellsam():
 
 
 def run_default_instanseg():
-    # Get the image paths for LIVECell.
-    data_dir = "/mnt/vast-nhr/projects/cidas/cca/data/livecell"
-    image_paths, label_paths = get_livecell_paths(path=data_dir, split="test")
+    image_paths, label_paths = get_image_label_paths(dataset_name=NAME, split="test")
 
     msas, sa50s, precisions, recalls, f1s = [], [], [], [], []
     for curr_image_path, curr_label_path in tqdm(
@@ -236,37 +221,9 @@ def run_default_instanseg():
     )
 
 
-def run_apg_grid_search(model_type="vit_b_lm"):
-    # Get the image paths for LIVECell.w
-    data_dir = "/mnt/vast-nhr/projects/cidas/cca/data/livecell"
-    val_image_paths, val_label_paths = get_livecell_paths(path=data_dir, split="val")
-    test_image_paths, _ = get_livecell_paths(path=data_dir, split="test")
-
-    # HACK: Let's test the pipeline super quickly on a very few validation images.
-    val_image_paths, val_label_paths = val_image_paths[:10], val_label_paths[:10]
-
-    experiment_folder = "./experiments/livecell"  # HACK: Hard-coded to something random atm.
-    prediction_folder = run_apg(
-        checkpoint=None,
-        model_type=model_type,
-        experiment_folder=experiment_folder,
-        val_image_paths=val_image_paths,
-        val_gt_paths=val_label_paths,
-        test_image_paths=test_image_paths,
-    )
-
-    # Get the prediction paths
-    prediction_paths = sorted(glob(os.path.join(prediction_folder, "*.tif")))
-    _, label_paths = _get_livecell_paths(input_folder=data_dir)
-    res = run_evaluation(label_paths, prediction_paths, os.path.join(experiment_folder, "results", "apg.csv"))
-    print(res)
-
-
 def main():
-    run_apg_grid_search()
-
-    # run_default_ais()
-    run_default_apg()
+    run_default_ais()
+    # run_default_apg()
     # run_default_amg("vit_b")
     # run_default_amg("vit_b_lm")
 
