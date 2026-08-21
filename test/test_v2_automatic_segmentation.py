6dd08bec37b12e781215ce6dd2a17895ac7c8e66
Anwai Archit <anwai.archit@gmail.com>
2026-08-21 12:54:57 +0200

    Add more coverage tests

--- test/test_v2_automatic_segmentation.py
+++ test/test_v2_automatic_segmentation.py
@@ -647,7 +647,7 @@ def test_get_decoder_preserves_the_peft_encoder_architecture(tmp_path, monkeypat
     from micro_sam.v2.models import util as models_util
 
     class FakeUniSAM2:
-        def __init__(self, encoder, output_channels):
+        def __init__(self, encoder, output_channels, initial_features=64):
             self.encoder = encoder
 
         def load_state_dict(self, state):
@@ -668,7 +668,10 @@ def test_get_decoder_preserves_the_peft_encoder_architecture(tmp_path, monkeypat
     monkeypatch.setattr(peft_sam2, "PEFT_Sam2", lambda model, **kwargs: types.SimpleNamespace(sam=model))
 
     checkpoint = tmp_path / "decoder.pt"
-    torch.save({"model_state": {}, "peft_kwargs": {"rank": 2, "peft_module": "LoRASurgery"}}, checkpoint)
+    model_state = {"out_conv.weight": torch.zeros(4, 64, 1, 1, 1)}
+    torch.save(
+        {"model_state": model_state, "peft_kwargs": {"rank": 2, "peft_module": "LoRASurgery"}}, checkpoint
+    )
 
     decoder = get_decoder("hvit_l", checkpoint=checkpoint, encoder=object())
 
@@ -683,13 +686,14 @@ def test_get_unisam2_model_converts_automatic_qlora_state(tmp_path, monkeypatch)
     from micro_sam.v2.instance_segmentation import get_unisam2_model
 
     class FakeUniSAM2(torch.nn.Module):
-        def __init__(self, encoder, output_channels):
+        def __init__(self, encoder, output_channels, initial_features=64):
             super().__init__()
             self.encoder = torch.nn.Module()
             self.encoder.inner = torch.nn.Module()
             self.encoder.inner.frozen_weight = torch.nn.Parameter(torch.tensor([1.0, 2.0]), requires_grad=False)
             self.encoder.inner.adapter_weight = torch.nn.Parameter(torch.tensor([0.0]))
             self.decoder_weight = torch.nn.Parameter(torch.tensor([0.0]))
+            self.out_conv = torch.nn.Conv3d(initial_features, output_channels, 1)
 
     monkeypatch.setattr(v2_util, "get_sam2_model", lambda **kwargs: types.SimpleNamespace(image_encoder=object()))
     monkeypatch.setattr(models_util, "UniSAM2", FakeUniSAM2)
@@ -703,6 +707,8 @@ def test_get_unisam2_model_converts_automatic_qlora_state(tmp_path, monkeypatch)
                 "encoder.inner.frozen_weight.quant_state.bitsandbytes__nf4": torch.tensor([8]),
                 "encoder.inner.adapter_weight": torch.tensor([3.0]),
                 "decoder_weight": torch.tensor([4.0]),
+                "out_conv.weight": torch.zeros(4, 64, 1, 1, 1),
+                "out_conv.bias": torch.zeros(4),
             },
             "peft_kwargs": {"rank": 2, "peft_module": "LoRASurgery", "quantize": True},
         },
